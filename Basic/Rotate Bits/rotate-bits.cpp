866086b6213a01ef4eb3bf0af9210b58aaf5f9e2
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-09-20 19:23:42 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Rotate Bits/rotate-bits.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+        vector <int> rotate (int n, int d)
+        {
+            d = d%16;
+           unsigned short int a = ((n<<d) | (n>>(16-d))) ;
+            unsigned short int b = ((n>>d) | (n<<(16-d))) ;
+            
+            return {a,b};
+        }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        
+        int n, d; cin >> n >> d;
+        Solution ob;
+        vector <int> res = ob.rotate (n, d);
+        cout << res[0] << endl << res[1] << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
