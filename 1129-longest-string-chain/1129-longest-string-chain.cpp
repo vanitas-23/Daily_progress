a3ea3fe54decf75eb8cbff3b1080b5035ac1d854
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-09-23 13:35:05 +0530

    Time: 356 ms (29.04%), Space: 149.8 MB (25.17%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    unordered_map<string,vector<string>>mp;
+    unordered_map<string,int>dp;
+    int dfs(string i){
+    
+       int ans=1;
+       if(mp[i].size()==0)
+       return dp[i]=ans;
+       if(dp.find(i)!=dp.end())
+       return dp[i];
+       int mx=0;
+       for(auto it:mp[i])
+       mx=max(dfs(it),mx);
+       return dp[i]=ans+mx;
+    }
+unordered_set<string> gen(string s) {
+    unordered_set<string> result;
+
+    for (int i = 0; i < s.length(); i++) {
+        string newString = s.substr(0, i) + s.substr(i + 1);
+        result.insert(newString);
+    }
+
+    return result;
+}
+    int longestStrChain(vector<string>& words) {
+       
+        int mx=0;
+        for(auto i:words){
+            mx=max(mx,(int)i.size());
+            
+            auto x=gen(i);
+            
+            vector<string>res(x.begin(),x.end());
+            mp[i]=res;
+        }
+        int ans=0;
+        
+        for(auto i:words){
+            ans=max(dfs(i),ans);
+        }
+        return ans-1;
+    }
+};
