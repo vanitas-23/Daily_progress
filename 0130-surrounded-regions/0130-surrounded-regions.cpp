ce3327ddc6dda1a160370eea5c1c8a16b6b0458f
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-09-29 16:18:19 +0530

    Time: 15 ms (18.23%), Space: 10.7 MB (24.87%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,39 +1,75 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<char>>& board,int* dx,int* dy,int n,int m){
-        board[i][j]='m';
-        for(int k=0;k<4;k++){
-            int x=i+dx[k];
-            int y=j+dy[k];
-            if(x>=0 && x<n && y>=0 && y<m && board[x][y]=='O')
-            dfs(x,y,board,dx,dy,n,m);
+    int parent[40005];
+    int rank[40005];
+
+    void init(){
+        for(int i=0; i<40005; i++){
+            parent[i] = i;
+            rank[i] = 0;
         }
     }
+
+    int findParent(int u){
+        if(parent[u] == u)
+            return u;
+        return parent[u] = findParent(parent[u]);
+    }
+
+    void unionf(int u, int v){
+        int up = findParent(u);
+        int vp = findParent(v);
+
+        if(up == vp)
+            return;
+        if(rank[up] == rank[vp]){
+            parent[up] = vp;
+            rank[vp]++;
+        } else if (rank[up] > rank[vp]){
+            parent[vp] = up;
+        } else {
+            parent[up] = vp;
+        }
+    }
+
     void solve(vector<vector<char>>& board) {
-        int dx[4]={-1,1,0,0};
-        int dy[4]={0,0,1,-1};
-        
-        int n=board.size();
-        int m=board[0].size();
+        int m = board.size();
+        int n = board[0].size();
 
-        for(int i=0;i<n;i++){
-            if(board[i][0]=='O')
-            dfs(i,0,board,dx,dy,n,m);
-            if(board[i][m-1]=='O')
-            dfs(i,m-1,board,dx,dy,n,m);
-        }
+        init();
+        rank[40004]= INT_MAX;
 
-        for(int i=0;i<m;i++){
-            if(board[0][i]=='O')
-            dfs(0,i,board,dx,dy,n,m);
-            if(board[n-1][i]=='O')
-            dfs(n-1,i,board,dx,dy,n,m);
+        for(int i=0; i<m; i++){
+            if(board[i][0] == 'O')
+                unionf(i*n ,40004);
+            if(board[i][n-1] == 'O')
+                unionf(i*n+n-1, 40004);
         }
-        for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-        if(board[i][j]=='m')
-        board[i][j]='O';
-        else
-        board[i][j]='X';
+        for(int j=1; j<n-1; j++){
+            if(board[0][j] == 'O')
+                unionf(j, 40004);
+            if(board[m-1][j] == 'O')
+                unionf(n*(m-1)+j,40004);
+        }
+        for(int i=1; i<m-1; i++){
+            for(int j=1; j<n-1; j++){
+                if(board[i][j] == 'X')
+                    continue;
+                if(board[i-1][j] == 'O')
+                    unionf((i-1)*n+j, i*n+j);
+                if(board[i+1][j] == 'O')
+                    unionf((i+1)*n+j, i*n+j);
+                if(board[i][j-1] == 'O')
+                    unionf((i)*n+j-1, i*n+j);
+                if(board[i][j+1] == 'O')
+                    unionf((i)*n+j+1, i*n+j);
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j] == 'O' && findParent(i*n+j) != 40004)
+                    board[i][j] = 'X';
+            }
+        }
     }
 };
