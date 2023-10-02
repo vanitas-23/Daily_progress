c8efb3b07201942bb1156425ae8caa092718d28d
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-02 09:34:29 +0530

    Time: 214 ms (99.81%), Space: 111.1 MB (99.81%) - LeetHub

--- 2388-replace-elements-in-an-array/2388-replace-elements-in-an-array.cpp
+++ 2388-replace-elements-in-an-array/2388-replace-elements-in-an-array.cpp
@@ -1,13 +1,13 @@
+int m[1000001];
 class Solution {
 public:
-    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& op) {
-        unordered_map<int,int>mp;
-       for(int i=0 ; i < nums.size() ; i++) 
-       mp[nums[i]] = i; 
-        for(vector<int> a : op) mp[a[1]] = mp[a[0]] , mp[a[0]] = -1; 
-        for(auto i : mp)
-            if(i.second >= 0) 
-                nums[i.second] = i.first;
-        return nums;
+vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+    for (int i = 0; i < nums.size(); ++i)
+        m[nums[i]] = i;
+    for (auto &op : operations) {
+        nums[m[op[0]]] = op[1];
+        m[op[1]] = m[op[0]];
     }
+    return nums;
+}
 };
