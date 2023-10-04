c3f3bd11d3666400495d8ce21eb715d68b14860a
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-04 07:47:01 +0530

    Time: 116 ms (47.64%), Space: 63.9 MB (46.91%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -1,30 +1,91 @@
-class MyHashMap {
+class Node{
 public:
-    int mp[1000001];    
-
-    MyHashMap() {
-        for (int i = 0; i < 1000001; ++i) {
-            mp[i] = -1;
-        }
-    }
+    int val;
+    int key;
+    Node* left = NULL;
+    Node* right = NULL;
     
-    void put(int key, int value) {
-        mp[key] = value;
-    }
-    
-    int get(int key) {
-        return mp[key];
-    }
-    
-    void remove(int key) {
-        mp[key] = -1;
+    Node(int key, int val){
+        this->key = key;
+        this->val = val;
     }
 };
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
+class MyHashMap {
+private:
+    Node* root;
+    
+    Node* helpPut(Node* root, int key, int val){
+        if(root == NULL)
+            return new Node(key, val);
+        if(root->key == key)
+            root->val = val;
+        else if(root->key > key)
+            root->left = helpPut(root->left, key, val);
+        else
+            root->right = helpPut(root->right, key, val);
+        return root;    
+    }
+    
+    int helpGet(Node* root, int key){
+        if(root == NULL) return -1;
+        if(root->key == key) return root->val;
+        if(root->key > key) return helpGet(root->left, key);
+        return helpGet(root->right, key);
+    }
+    
+    Node* helpDelete(Node* root, int key){
+        if(root == NULL) return NULL;
+        
+        if(root->key > key)
+            root->left = helpDelete(root->left, key);
+        else if(root->key < key)
+            root->right = helpDelete(root->right, key);
+        else{
+            if(root->left == NULL && root->right == NULL){
+                return NULL;
+            }
+            else if(root->left == NULL){
+                auto temp = root->right;
+                return temp;
+            }
+            else if(root->right == NULL){
+                auto temp = root->left;
+                return temp;
+            }
+            else{
+                auto m = getMin(root->right);
+                root->key = m->key;
+                root->val = m->val;
+                root->right = helpDelete(root->right, m->key);
+            }
+        }
+        return root;
+    }
+    
+    Node* getMin(Node* root){
+        auto c = root;
+        while(c && c->left){
+            c = c->left;
+        }
+        return c;
+    }
+    
+public:
+    MyHashMap() {
+        root = (Node *)nullptr;
+    }
+    
+    void put(int key, int value) {
+        root = helpPut(root, key, value);
+    }
+    
+    
+    int get(int key) {
+        return helpGet(root, key);
+    }
+    
+    void remove(int key) {
+        root = helpDelete(root, key);
+    }
+};
