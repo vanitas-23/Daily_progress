4b464310b6bbfbdca89173bb01fe953a4c173e62
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-04 20:47:23 +0530

    Time: 37 ms (21.52%), Space: 20.6 MB (15.63%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    bool is(string s){
-        string r=s;
-        reverse(r.begin(),r.end());
-        return r==s;
+int solveMem(string& s, int i, int j, vector<vector<int>>& dp)  {
+        if(i>j) return 1;
+        if(dp[i][j] != -1)  return dp[i][j];
+        return dp[i][j] = (s[i]==s[j]) ? solveMem(s, i+1, j-1, dp) : 0;
     }
     int countSubstrings(string s) {
-        int cnt=0;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            cnt++;
-            string res="";
-            res+=s[i];
-            for(int j=i+1;j<n;j++){
-                res+=s[j];
-                if(is(res))
-                cnt++;
-            }
-        }
+       int cnt = 0, n=s.length();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        for(int i=0; i<n; i++)
+            for(int j=i; j<n; j++)
+                
+                cnt += solveMem(s, i, j, dp);
         return cnt;
     }
 };
