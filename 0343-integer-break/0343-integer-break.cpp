0d96981b7707e815b2cef657a2893927a391013e
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-06 07:17:24 +0530

    Time: 0 ms (100%), Space: 6.5 MB (29.03%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int f(int n,vector<int>& dp){
-       
-        if(n==0)
-        return 1;
-        if(n<0)
-        return -1;
-        if(dp[n]!=-1)
-        return dp[n];
-        int ans=-1;
-        for(int i=1;i<=n;i++)
-        ans=max(ans,i*f(n-i,dp));
-        return dp[n]=ans;
-    }
     int integerBreak(int n) {
-        if(n==2)
-        return 1;
-        if(n==3)
-        return 2;
-        vector<int>dp(n+1,-1);
-        return f(n,dp);
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        
+        int count_of_3s = n / 3;
+        int remainder = n % 3;
+        
+        if (remainder == 0) 
+            return pow(3, count_of_3s);
+         if (remainder == 1) 
+            return pow(3, count_of_3s - 1) * 4;
+        
+            return pow(3, count_of_3s) * 2;
+        
     }
 };
