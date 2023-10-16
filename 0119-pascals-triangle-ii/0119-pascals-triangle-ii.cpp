1be1b64873f4c72753f57ab56cc7162611f72db3
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-16 06:37:55 +0530

    Time: 0 ms (100%), Space: 6.6 MB (51.35%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    vector<int> f(vector<int>res ,int i,int r){
-        if(i==r)
+    std::vector<int> getRow(int rowIndex) {
+        std::vector<int> res(1, 1);
+        long long prev = 1;
+        for(int k = 1; k <= rowIndex; k++) {
+            long long next_val = prev * (rowIndex - k + 1) / k;
+            res.push_back(next_val);
+            prev = next_val;
+        }
         return res;
-        int n=res.size();
-        vector<int>dum(n+1);
-        dum[0]=res[0];
-        dum[n]=res[n-1];
-        for(int i=1;i<n;i++)
-        dum[i]=res[i-1]+res[i];        
-        return f(dum,i+1,r);
-    }
-    vector<int> getRow(int r) {
-        return f({1},0,r);
     }
 };
