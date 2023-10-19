b8490ab9d23b02e1a3b7459a34ee16ab7cc8a289
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-19 12:04:27 +0530

    Time: 0 ms (100%), Space: 6.5 MB (54.78%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,24 +1,24 @@
+
+
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        string st="";
-        string rt="";
-        for(char c:s){
-            if(c=='#'){
-                if(st.size()>0)
-                st.pop_back();
+        remove_letters(s);
+        remove_letters(t);
+        return s == t;
+    }
+    void remove_letters(std::string& s){
+        auto it = std::find(s.begin(), s.end(), '#');
+        while(it != s.end()){
+            if(it == s.begin()){
+                s.erase(it);
+                it = std::find(s.begin(), s.end(), '#');
                 continue;
             }
-            st+=c;
+            auto prev = it - 1;
+            s.erase(it);
+            s.erase(prev);
+            it = std::find(s.begin(), s.end(), '#');
         }
-        for(char c:t){
-            if(c=='#'){
-                if(rt.size()>0)
-                rt.pop_back();
-                continue;
-            }
-            rt+=c;
-        }
-        return st==rt;
     }
 };
