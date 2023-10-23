4ec2fde0a6ee8c66b234b5c8ceb6df1a7d12ccbd
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-23 19:40:57 +0530

    Time: 51 ms (25.69%), Space: 55.7 MB (47.65%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-    int longestOnes(vector<int>& arr, int k) {
-        int n = arr.size();
-
-        vector<int> prefix(n , 0);
-        prefix[0] = (arr[0]==0);
-
-        for(int i=1 ; i<n ; i++){
-            prefix[i] = prefix[i-1] + (arr[i]==0);
+    int longestOnes(vector<int>& nums, int k) {
+        int i=0;
+        int j;
+        for( j=0;j<nums.size();j++){
+                if(nums[j]==0)
+                k--;
+                if(k<0){
+                    if(nums[i]==0)
+                    k++;
+                    i++;
+                }
         }
-
-        int maxx = 0;
-
-        if(prefix[n-1]<=k) return n;
-
-        for(int i=0 ; i<n ; i++){
-            int x = prefix[i];
-            auto it = upper_bound(prefix.begin() , prefix.end() , x+k);
-            int j = it-prefix.begin();
-            if(arr[i] == 0)
-maxx = max(maxx , j-i-1);
-else
-maxx = max(maxx, j-i);
-        }
-
-
-        return maxx;
+        return j-i;
     }
 };
