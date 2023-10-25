01f79a7929858cf372336a44e2823e63945c7c13
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-25 19:53:20 +0530

    Time: 42 ms (100%), Space: 52 MB (99.92%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
@@ -9,20 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ const static auto fast = [] {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
-    int sum=0;
-    void f(TreeNode* root,int low,int high){
-        if(!root)
-        return ;
-        f(root->left,low,high);
-        if(root->val >= low && root->val<=high)
-        sum+=root->val;
-
-        f(root->right,low,high);
-    }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        f(root,low,high);
+        int sum=0;
+        while(root){
+            if(root->left){
+                TreeNode* temp=root->left;
+                TreeNode* curr=root->left;
+                while(temp->right){
+                    temp=temp->right;
+                }
+                temp->right=root;
+                root->left=NULL;
+                root=curr;
+            }
+            else{
+                if(root->val>=low&&root->val<=high){
+                    sum+=root->val;
+                }
+                root=root->right;
+            }
+        }
         return sum;
     }
 };
