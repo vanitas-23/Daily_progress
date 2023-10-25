ea00b8b91b13692d87b43eec7db77d2e8d7c57f1
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-25 08:30:09 +0530

    Time: 0 ms (100%), Space: 6.5 MB (12.22%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        if(n==1)
-        return 0;
-        int x=pow(2,n-1);
-        if(k==x)
-        return n%2==0;
-
-        if(k>=pow(2,n-1))
-        return !kthGrammar(n-1,k%x);
-
-        return kthGrammar(n-1,k%x);
-
+        int count = __builtin_popcount(k - 1);
+        return count%2;
     }
 };
