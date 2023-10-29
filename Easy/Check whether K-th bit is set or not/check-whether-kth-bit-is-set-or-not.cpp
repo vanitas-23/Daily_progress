2a648e5dcc638dc2f3e07c2336cdd586eff2a844
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-10-29 07:37:39 +0530

    Updated Solution - GfG to GitHub

--- Easy/Check whether K-th bit is set or not/check-whether-kth-bit-is-set-or-not.cpp
+++ Easy/Check whether K-th bit is set or not/check-whether-kth-bit-is-set-or-not.cpp
@@ -15,9 +15,7 @@ class Solution
     // Function to check if Kth bit is set or not.
     bool checkKthBit(int n, int k)
     {
-        while(k--)
-        n=n>>1;
-        return (n&1)==1;
+        return n&(1<<k);
     }
 };
 
