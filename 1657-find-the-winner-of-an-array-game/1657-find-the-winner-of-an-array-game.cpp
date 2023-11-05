f8bf014cecfe18bcecb6148fae1b01dd77e76564
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-11-05 07:42:22 +0530

    Time: 96 ms (30.56%), Space: 66.2 MB (25.93%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
-    int getWinner(std::vector<int>& arr, int k) {
-        if (k == 1) {
-            return std::max(arr[0], arr[1]);
-        }
-        if (k >= arr.size()) {
-            return *std::max_element(arr.begin(), arr.end());
-        }
-
-        int current_winner = arr[0];
-        int consecutive_wins = 0;
-
-        for (int i = 1; i < arr.size(); ++i) {
-            if (current_winner > arr[i]) {
-                consecutive_wins++;
-            } else {
-                current_winner = arr[i];
-                consecutive_wins = 1;
+    int getWinner(vector<int>& arr, int k) {
+        if (k == 1) 
+            return max(arr[0], arr[1]);
+        if (k >= arr.size()) 
+            return *max_element(arr.begin(), arr.end());
+        
+        vector<int>d(arr.begin(),arr.end());
+        stack<int>st;
+        int n=arr.size();
+        int c=0;
+        int mx=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(c==k)
+            return st.top();
+            if(c>mx)
+                    ans=st.top(),mx=c;
+            if(c==mx && mx!=0)
+            ans=st.top(),mx=c;
+            if(st.empty())
+            st.push(d[i]);
+            else {
+                if(st.top()>d[i])
+                c++;
+                else{
+                    
+                    st.pop();
+                    st.push(d[i]);
+                    c=1;
+                }
             }
-
-            if (consecutive_wins == k) {
-                return current_winner;
-            }
         }
+        if(c>mx)
+                    ans=st.top(),mx=c;
+            if(c==mx && mx!=0)
+            ans=st.top(),mx=c;
 
-        return current_winner;
+        if(c!=k)
+        return *max_element(arr.begin(), arr.end());
+        return ans;
     }
 };
