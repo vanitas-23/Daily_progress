ae53e4cb2ae02ec44132f44a37ad7d8f7f3c9b6e
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-11-19 11:20:07 +0530

    Time: 147 ms (89.57%) | Memory: 83.1 MB (88.96%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
-        sort(nums.rbegin(),nums.rend());
-        int top=-1;
+        sort(nums.begin(),nums.end());
+        int c=1;
         int ans=0;
-        int cnt=0;
-        for(int i:nums){
-            
-                    if(top!=i)
-                    ans+=cnt;
-                    top=i;
-
-            cnt++;
+        int l=nums[nums.size()-1];
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]==l){
+                c++;
+            }
+            else{
+                ans+=c;
+                c++;
+                l=nums[i];
+            }
         }
         return ans;
-
     }
 };
