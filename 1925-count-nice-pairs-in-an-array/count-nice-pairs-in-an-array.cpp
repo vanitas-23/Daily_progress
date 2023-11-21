42ea6b12502daf26fc5de39c65164b8a89b18a7e
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-11-21 15:09:25 +0530

    Time: 79 ms (97.05%) | Memory: 57.4 MB (35.81%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
-int rev(int num) {
-        int result = 0;
-        while (num > 0) {
-            result = result * 10 + num % 10;
-            num /= 10;
-        }
-        
-        return result;
-    }
-    int mod=1e9+7;
     int countNicePairs(vector<int>& nums) {
-        vector<int>arr=nums;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-            arr[i]=rev(nums[i]);
-        
-        for(int i=0;i<n;i++)
-        arr[i]-=nums[i];
-      //  cout<<n<<" ";
-        sort(arr.begin(),arr.end());
-        int ans=0;
-        int curr=arr[0];
-        int temp=1;
-        for(int i=1;i<n;i++){
-            if(arr[i]==curr)
-            temp++;
-            else{
-                curr=arr[i];
-                ans = (ans%mod + (temp*(temp-1))/2)%mod;
-                temp=1;
+       
+        unordered_map<int,int> mp;
+        long long ans = 0;
+        for(int num:nums) {
+            int dup = num;
+            int rev = 0;
+            while(dup>0) {
+                rev = rev*10 + dup%10;
+                dup = dup/10;
             }
+            ans+= mp[(num-rev)];
+            ans = ans%1000000007;
+            mp[(num-rev)]++;
         }
-     //   cout<<temp;
-        ans = (ans%mod + (temp*1LL*(temp-1))/2)%mod;
         return ans;
     }
 };
