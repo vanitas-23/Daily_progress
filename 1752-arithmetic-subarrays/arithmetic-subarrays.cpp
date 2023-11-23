e4249a93f362eea058be404a471b34bd8ddfc583
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-11-23 23:04:46 +0530

    Time: 30 ms (94.32%) | Memory: 29.1 MB (60.23%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.cpp
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     bool check(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        int diff = arr[1] - arr[0];
-        
-        for (int i = 2; i < arr.size(); i++) {
-            if (arr[i] - arr[i - 1] != diff) {
-                return 0;
-            }
+        int mn=INT_MAX;
+        int mx=INT_MIN;
+        for(int i:arr){
+            mn=min(i,mn);
+            mx=max(i,mx);
+        }
+        int n=arr.size();
+        if((mx-mn)%(n-1)!=0)
+        return 0;
+
+        int d=(mx-mn)/(n-1);
+        for(auto& i:arr)
+        i-=mn;
+
+        if(d==0)
+        return mn==mx;
+        unordered_set<int>st;
+        for(int i:arr){
+        if(i%d!=0)
+        return 0;
+        if(st.find(i/d)!=st.end())
+        return 0;
+        st.insert(i/d);
         }
         return 1;
     }
     vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
         vector<bool> ans;
+        
         for (int i = 0; i < l.size(); i++) {
             vector<int> arr(begin(nums) + l[i], begin(nums) + r[i] + 1);
             ans.push_back(check(arr));
