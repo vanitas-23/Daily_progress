fa2f5186de88a75f59f9252b49e3bfdb87025a77
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-12-14 21:14:48 +0530

    Time: 223 ms (5.45%) | Memory: 86.8 MB (10.90%) - LeetSync

---
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+       int req=0;
+       priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.end());
+    
+
+        while(pq.size()){
+
+            int curr=pq.top();
+            pq.pop();
+            while(pq.top()<=curr+k && pq.size()>0)
+            pq.pop();
+           // cout<<curr<<" ";
+            req++;
+        }
+        return req;
+    }
+};
