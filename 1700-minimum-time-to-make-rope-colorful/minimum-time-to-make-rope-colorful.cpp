2e86b210cc3c0a6d300a502d492ac67378693b8d
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-12-27 17:36:47 +0530

    Time: 117 ms (64.23%) | Memory: 95.7 MB (83.31%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-  int minimum_time(string &colors,vector<int>&time,int idx,char  prev,vector<vector<int>>&dp)
-  {
-   if(idx>=colors.size())
-   {
-     return 0;
-   }
- 
-    if(dp[idx][prev-'a']!=-1)
-    {
-      return dp[idx][prev-'a'];
-    }
-   
-  int pick=time[idx]+minimum_time(colors,time,idx+1,prev,dp);
-   
-    
-    int not_pick=INT_MAX;
- 
-    if(colors[idx]!=prev)
-    {
-       
-      not_pick=0+minimum_time(colors,time,idx+1,colors[idx],dp);
-     
-    }
-
-    return dp[idx][prev-'a']=min(pick,not_pick);
-  }
-    int minCost(string colors, vector<int>&time) {
+    int minCost(string colors, vector<int>& neededTime) {
         
-      vector<vector<int>>dp(colors.size()+10,vector<int>(27,-1));
-    
-      return minimum_time(colors,time,0,'{',dp);
+        int count=0;
+        
+        for(int i=1;i<neededTime.size();i++)
+        {
+            if(colors[i]==colors[i-1]){
+
+                count += min(neededTime[i], neededTime[i - 1]); 
+                neededTime[i] = max(neededTime[i], neededTime[i - 1]); 
+
+            }
+        }
+        return count;
+
     }
 };
