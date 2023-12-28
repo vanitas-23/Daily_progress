fe128dcd2a6b3f1dede589b778f27fa6300ed19e
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-12-28 08:42:33 +0530

    Time: 32 ms (82.02%) | Memory: 6.4 MB (100.00%) - LeetSync

--- 1637-string-compression-ii/string-compression-ii.cpp
+++ 1637-string-compression-ii/string-compression-ii.cpp
@@ -1,27 +1,70 @@
-int dp[101][27][101][101];
 class Solution {
 public:
-    string str;
-    int n;
-    int minLength(int i, int prev, int len, int k)
-    {
-        if(k<0)    return INT_MAX;
-        if(i>=n)    return 0;
-        if(dp[i][prev][len][k] != -1)   return dp[i][prev][len][k];
-        int ifDelete = minLength(i+1,prev,len,k-1);
-        int keep = 0;
-        if(str[i]-'a' == prev)
-        {
-            if(len==1 || len==9 || len==99)     keep++;
-            keep += minLength(i+1, prev, len+1, k);
-        }
-        else    keep = 1 + minLength(i+1, str[i]-'a', 1, k);
-        return dp[i][prev][len][k] = min(ifDelete, keep);
+    
+    string s;
+    int N,K;
+    
+    int H(int x){
+        if(x==1)
+            return 1;
+        
+        if(x<10)
+            return 2;
+        if(x<100)
+            return 3;
+        return 4;
     }
-    int getLengthOfOptimalCompression(string s, int k) {
-        str = s;
-        n = s.size();
-        memset(dp,-1,sizeof(dp));
-        return minLength(0,26,0,k);
+    
+    int store[105][105];
+    int sol(int index,int k){
+        if(index==N){
+            return 0;
+        }
+        
+        if(store[index][k]!=-1)
+            return store[index][k];
+        
+        int ans=N;
+        
+        char prev=s[index];
+        int L=0;
+        int tempk=k;
+        
+        if(tempk>0){
+            ans=min(ans,sol(index+1,tempk-1));
+        }
+        
+        for(int i=index;i<N;i++){
+            if(s[i]==prev){
+                L++;
+                ans=min(ans,H(L)+sol(i+1,tempk));
+            } 
+            else{
+                ans=min(ans,H(L)+sol(i,tempk));
+                if(tempk>0)
+                    tempk--;
+                else
+                    break;
+            }
+            
+            
+        }
+        
+        
+        
+        return store[index][k]=ans;
+        
+    }
+    int getLengthOfOptimalCompression(string t, int k) {
+        s=t;
+        N=(int)s.size();
+        K=k;
+
+        for(int i=0;i<=N;i++){
+            for(int j=0;j<=N;j++)
+                store[i][j]=-1;
+        }
+        
+        return sol(0,k);
     }
 };
