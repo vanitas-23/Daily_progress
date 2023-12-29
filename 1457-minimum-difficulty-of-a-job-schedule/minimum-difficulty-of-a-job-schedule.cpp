2274e77dfd430342f4c2ad29a41315f0038bbb9e
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2023-12-29 14:05:04 +0530

    Time: 269 ms (5.06%) | Memory: 8.3 MB (23.61%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -21,22 +21,22 @@ struct Tree {
 class Solution {
 public:
     
-    int f(vector<int>& j, int d,int i,int n,Tree& t,int pre,vector<vector<vector<int>>>& dp){
+    int f(vector<int>& j, int d,int i,int n,Tree& t,int pre,vector<vector<int>>& dp){
         if(i == n && d == 0) return 0;
         if(i == n || d == 0) return 1e9;
-        if(dp[i][d][pre+1]!=-1)
-        return dp[i][d][pre+1];
+        if(dp[d][pre+1]!=-1)
+        return dp[d][pre+1];
         int ntaken=t.query(pre+1,i+1)+f(j,d-1,i+1,n,t,i,dp);
         int taken=f(j,d,i+1,n,t,pre,dp);
 
-        return dp[i][d][pre+1]=min(ntaken,taken);
+        return dp[d][pre+1]=min(ntaken,taken);
     }
     int minDifficulty(vector<int>& j, int d) {
         int n=j.size();
         Tree t(n,0);
         for(int i=0;i<n;i++)
         t.update(i,j[i]);
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(d+1,vector<int>(n+1,-1)));
+        vector<vector<int>>dp(d+1,vector<int>(n+1,-1));
         return f(j,d,0,n,t,-1,dp)>=1e9?-1:f(j,d,0,n,t,-1,dp);
     }
 };
