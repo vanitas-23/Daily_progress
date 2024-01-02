161f660cbaad449d7bdaae3953188dad42f4e78a
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-01-02 13:57:24 +0530

    Time: 24 ms (8.02%) | Memory: 30.6 MB (15.92%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,23 +1,29 @@
 class Solution {
-    
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
-   // cout << fixed << setprecision(12);
- 
+        vector<vector<int>> ans;
         unordered_map<int,int>mp;
-        for(int i:nums)
-        mp[i]++;
-        int mx=0;
-        for(auto i:mp)
-        mx=max(mx,i.second);
-       // vector<unordered_set<int>>ch;
-        vector<vector<int>>res(mx);
-        for(auto i:mp){
-            for(int j=0;j<i.second;j++)
-            res[j].push_back(i.first);
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
         }
-        return res;
+        while(mp.size()>0){
+            vector<int> v;
+            vector<int> f;
+            for(auto it=mp.begin();it!=mp.end();it++){
+                cout<<mp.size();
+                v.push_back(it->first);
+                int x=it->second;
+                mp[it->first]=x-1;
+                if(x-1==0){
+                   f.push_back(it->first);
+                }
+            }
+            for(int i=0;i<f.size();i++){
+                mp.erase(f[i]);
+            }
+            ans.push_back(v);
+          //  cout<<mp.size()<<" ";
+        }
+        return ans;
     }
 };
