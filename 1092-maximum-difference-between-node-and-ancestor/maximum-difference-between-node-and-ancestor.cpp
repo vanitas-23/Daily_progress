f2a0755ecab4e9a5edb661cf8de9f721f61f6b82
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-01-11 08:57:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (10.26%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -1,43 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int maxAncestorDiff(TreeNode* root) {
-        using Node=TreeNode;
-        unordered_map<Node*,Node*>mp;
-        mp[root]=nullptr;
-        queue<Node*>q;
-        q.push(root);
-        int ans=-1;
-        while(!q.empty()){
-            auto x=q.front();
-            q.pop();
-            auto y=x;
-            while(mp[x]!=nullptr)
-            {
-                ans=max(ans,abs(y->val-mp[x]->val));
-                x=mp[x];
-            }
-            if(y->left)
-            {
-                mp[y->left]=y;
-                q.push(y->left);
-            }
-            if(y->right)
-            {
-                mp[y->right]=y;
-                q.push(y->right);
-            }
+        int m = 0;
+        dfs(root, m);
+        return m;
+    }
+
+private:
+    std::pair<int, int> dfs(TreeNode* root, int& m) {
+        if (root == nullptr) {
+            return {INT_MAX, INT_MIN};
         }
-        return ans;
+
+        auto left = dfs(root->left, m);
+        auto right = dfs(root->right, m);
+
+        int minVal = min({root->val,left.first,right.first});
+        int maxVal = max({root->val,left.second, right.second});
+
+        m = max({m,abs(minVal - root->val),abs(maxVal - root->val)});
+
+        return {minVal, maxVal};
     }
 };
+
+
