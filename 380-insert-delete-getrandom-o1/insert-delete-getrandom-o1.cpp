912dd476a8e9c88bca3e65d5f27fee127e673b2d
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-01-16 08:11:36 +0530

    Time: 134 ms (98.27%) | Memory: 95.4 MB (96.50%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,29 +1,39 @@
 class RandomizedSet {
 public:
-    unordered_set<int>st;
+    unordered_map<int, int> mp;
+    vector<int> ans;
     RandomizedSet() {
-        
+        ios_base::sync_with_stdio(false);
     }
     
     bool insert(int val) {
-        if(st.find(val)!=st.end())
-        return false;
-        st.insert(val);
+        if(mp.find(val)!=mp.end())
+        
+            return false;
+            
+        ans.push_back(val);
+        mp[val]=ans.size()-1;
         return true;
     }
     
     bool remove(int val) {
-        if(st.find(val)==st.end())
-        return false;
-        st.erase(val);
+        if(mp.find(val)==mp.end())
+        
+            return false;
+        
+        int index = mp[val];
+        int last_ele = ans.back();
+
+        mp[last_ele] = index;
+        swap(ans[index], ans[ans.size()-1]);
+
+        ans.pop_back();
+        mp.erase(val);
         return true;
     }
     
     int getRandom() {
-        int n=st.size();
-        int i=rand()%n;
-        auto it = next(st.begin(), i);
-        return *it;
+        return ans[rand()%ans.size()];
     }
 };
 
