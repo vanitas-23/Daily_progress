0243b5ccea50cfc57efcde63ad5902144516c6b5
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-01-24 19:43:36 +0530

    Time: 3 ms (71.13%) | Memory: 8 MB (6.84%) - LeetSync

---
+++ 768-partition-labels/partition-labels.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int>frq(26);
+        for(char c:s)
+        frq[c-'a']++;
+        vector<int>res;
+        string temp="";
+        for(char c:s)
+        {
+            temp+=c;
+            frq[c-'a']--;
+            bool b=1;
+            for(char l:temp)
+            if(frq[l-'a']>0)
+            {
+                b=0;
+                continue;
+            }
+            if(!b)
+            continue;
+            res.push_back(temp.size());
+            temp="";
+        }
+        return res;
+    }
+};
