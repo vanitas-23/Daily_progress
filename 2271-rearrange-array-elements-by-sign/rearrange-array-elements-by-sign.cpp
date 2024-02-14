ca12447a1eea84f9327bebddaf3faddfccc047c2
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-02-14 08:00:13 +0530

    Time: 150 ms (67.65%) | Memory: 143.1 MB (5.30%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,49 +1,19 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>inD1;
-        vector<int>inD2;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>0)
-            {
-                if(inD1.size()==0)
-                inD1.push_back(0);
-                else
-                inD1.push_back(inD1.back()+1);
-            }
-            else
-            {
-                if(inD2.size()==0)
-                inD2.push_back(0);
-                else
-                inD2.push_back(inD2.back()+1);
-            }
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq1;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq2;
-        
-        int j=0,k=0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>0)
-            pq1.push({inD1[j++],nums[i]});
-            else
-            pq2.push({inD2[k++],nums[i]});
-        }
+        vector<int>p;
+        vector<int>n;
+        for(int i:nums)
+        if(i>0)
+        p.push_back(i);
+        else
+        n.push_back(i);
 
-    
+        int i=0;
         vector<int>res;
-        while(!pq1.empty() || !pq2.empty())
-        {
-            auto x=pq1.top();
-            pq1.pop();
-            auto y=pq2.top();
-            pq2.pop();
-            res.push_back(x.second);
-            res.push_back(y.second);
-        }
+        for(int i=0;i<p.size();i++)
+        res.push_back(p[i]),res.push_back(n[i]);
+
         return res;
     }
 };
