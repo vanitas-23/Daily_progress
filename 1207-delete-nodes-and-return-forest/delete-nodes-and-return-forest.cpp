306e50c8af6dd20a99a6f1b7238b52c5772f1900
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-04-02 22:05:58 +0530

    Time: 17 ms (46.41%) | Memory: 27.3 MB (19.44%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        queue<TreeNode*>q;
+        unordered_map<TreeNode*,TreeNode*>mp;
+        mp[root]=nullptr;
+        q.push(root);
+        while(!q.empty())
+        {
+            auto x = q.front();
+            q.pop();
+            if(x->left)
+            mp[x->left]=x,q.push(x->left);
+            if(x->right)
+            mp[x->right]=x,q.push(x->right);
+        }
+        unordered_set<int>st(to_delete.begin(),to_delete.end());
+        unordered_set<int>ress;
+        for(auto& i:mp)
+        {
+            if(st.find(i.first->val)!=st.end())
+            {
+                //cout<<i.first->val<<" ";
+                if(i.first->left)
+                mp[i.first->left]=nullptr;
+                if(i.first->right)
+                mp[i.first->right]=nullptr;
+                auto& x = mp[i.first];
+                if(x==nullptr)
+                {
+                    ress.insert(i.first->val);
+                    continue;
+                }
+                cout<<x->val<<" ";
+                if(x->left==i.first)
+                x->left=nullptr;
+                if(x->right==i.first)
+                x->right=nullptr;
+            }
+        }
+        vector<TreeNode*>res;
+        for(auto i:mp)
+        if(ress.find(i.first->val)==ress.end())
+        if(i.second==nullptr)
+        res.push_back(i.first);
+        return res;
+    }
+};
