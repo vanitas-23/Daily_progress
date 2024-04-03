e5645a6aaf2c32866a84d3a4ef12e65c85a357c7
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-04-03 20:45:34 +0530

    Time: 8 ms (79.01%) | Memory: 19.9 MB (43.51%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+		unordered_map<TreeNode*, int> depth;
+    
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        if(!root)
+            return NULL;
+        int l = countDepth(root->left);
+        int r = countDepth(root->right);
+        if(l == r)
+            return root;
+        else if(l < r)
+            return lcaDeepestLeaves(root->right);
+        else
+            return lcaDeepestLeaves(root->left);
+    }
+    
+    int countDepth(TreeNode* root){
+        if(!root)
+            return 0;
+        return 1+max(countDepth(root->left),countDepth(root->right));
+    }
+};
