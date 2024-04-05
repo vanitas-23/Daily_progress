a88d2c140ccf3cc6bd0b4106eeb814a17d156e23
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-04-05 07:52:46 +0530

    Time: 37 ms (89.98%) | Memory: 22.4 MB (78.74%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
@@ -1,27 +1,48 @@
+int delRow[] = {0, 0, -1, 1};
+int delCol[] = {1, -1, 0, 0};
+
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        vector<pair<int,int>>water,land;
-        int n=grid.size();
-        int m=grid[0].size();
-        for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-        if(grid[i][j])
-        land.push_back({i,j});
-        else
-        water.push_back({i,j});
+        queue<pair<int, int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
 
-        int ans=-1;
-        for(auto i:water)
-        {
-            int temp=1e9;
-            for(auto j:land)
-            if(abs(i.first-j.first)+abs(i.second-j.second)<temp)
-            {
-                temp=abs(i.first-j.first)+abs(i.second-j.second);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    q.push({i, j});
+                }
             }
-            ans=max(ans,temp);
         }
-        return ans==1e9?-1:ans;
+
+        int step = 0;
+
+        while (!q.empty()) {
+            step++;
+
+            int size = q.size(); // Get the current size of the queue
+
+            for (int k = 0; k < size; k++) {
+                auto it = q.front();
+                q.pop(); // Pop the front element
+
+                int row = it.first;
+                int col = it.second;
+
+                for (int i = 0; i < 4; i++) {
+                    int Nrow = delRow[i] + row;
+                    int Ncol = delCol[i] + col;
+
+                    if (Nrow >= 0 && Nrow < n && Ncol >= 0 && Ncol < m &&
+                        grid[Nrow][Ncol] == 0) {
+                        grid[Nrow][Ncol] = step;
+                        q.push({Nrow, Ncol}); // Push the new cell to the queue
+                    }
+                }
+            }
+        }
+
+        return step == 1 ? -1 : step - 1;
     }
 };
