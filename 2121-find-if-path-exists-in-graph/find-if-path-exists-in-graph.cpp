5985d1a93bb42f8e009d14adb87deae312cfa6bb
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-04-21 06:48:28 +0530

    Time: 311 ms (87.43%) | Memory: 137.5 MB (86.83%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -0,0 +1,33 @@
+class DisjointSet {
+    public:
+        DisjointSet(int n) : N(n), sz(n), pars(n, -1) {
+        }
+        void reset() { sz = N, fill(pars.begin(), pars.end(), -1); }
+        int find(int x) { return pars[x] < 0 ? x : pars[x] = find(pars[x]); }
+        int size() { return sz; }
+        int count(int x) { return -pars[find(x)]; }
+        bool unite(int x, int y) {
+            x = find(x), y = find(y);
+            if (x == y) { return false; }
+            --sz;
+            if (pars[x] < pars[y]) { swap(x, y); }
+            pars[y] += pars[x];
+            pars[x] = y;
+            return true;
+        }
+        bool isSame(int x, int y) { return find(x) == find(y); }
+    private:
+        int N, sz;
+        vector<int> pars;
+};
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int dest) {
+        DisjointSet ds(n+1);
+        for(auto i:edges)
+        ds.unite(i[0],i[1]);
+        if(ds.find(source)!=ds.find(dest))
+        return false;
+        return true;
+    }
+};
