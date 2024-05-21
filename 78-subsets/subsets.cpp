2009e6283c823d4cd32e201a3063b4d0cb0f57c2
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-05-21 18:12:29 +0530

    Time: 3 ms (47.78%) | Memory: 14.3 MB (17.89%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    set<vector<int>>st;
+
+    void f(vector<int>& nums,int ind,vector<int> v){
+        if(ind>=nums.size())
+        return ;
+        f(nums,ind+1,v);
+        st.insert(v);
+        v.push_back(nums[ind]);
+        f(nums,ind+1,v);
+        st.insert(v);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        f(nums,0,{});
+        vector<vector<int>>res(st.begin(),st.end());
+        return res;
+    }
+};
