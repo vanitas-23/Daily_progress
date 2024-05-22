8d055858e58e1efb4605f6c29270f2294e0e8557
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-05-22 17:54:52 +0530

    Time: 861 ms (29.54%) | Memory: 8.1 MB (48.53%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int solve(vector<int>&v,int i,vector<int>&a){
-        if(i==v.size()){
-            int ans=0;
-            for(auto it : a)ans=max(ans,it);
-            return ans;
+    int f(int i,int k,int n,vector<int>& cookies,vector<int>& arr)
+    {
+        if(i==n)
+        return *max_element(arr.begin(),arr.end());
+        int mn=1e9;
+        for(int id=0;id<k;id++)
+        {
+            arr[id]+=cookies[i];
+            mn=min(mn,f(i+1,k,n,cookies,arr));
+            arr[id]-=cookies[i];
         }
-        int minn=INT_MAX;
-        for(int j=0;j<a.size();j++){
-            a[j]+=v[i];
-            minn=min(minn,solve(v,i+1,a));
-            a[j]-=v[i];
-        }
-        return minn;
+        return mn;
     }
-    int distributeCookies(vector<int>&v, int k) {
-        int n=v.size();
-        vector<int>a(k);
-        return solve(v,0,a);}
+    int distributeCookies(vector<int>& cookies, int k) {
+        int n=cookies.size();
+        vector<int>arr(k);
+        return f(0,k,n,cookies,arr);
+    }
 };
