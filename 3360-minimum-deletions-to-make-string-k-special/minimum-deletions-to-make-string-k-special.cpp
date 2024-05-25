aae903a35f0890a9a3bedf2f5e981df9ffcef49c
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-05-25 18:59:59 +0530

    Time: 41 ms (74.51%) | Memory: 17.5 MB (53.61%) - LeetSync

---
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumDeletions(string word, int k) {
+        vector<int>arr(26);
+        for(char c:word)
+            arr[c-'a']++;
+        sort(arr.begin(),arr.end());
+        vector<int>p=arr;
+     
+        for(int i=24;i>=0;i--)
+            p[i]+=p[i+1];
+        long long ans=word.size();
+        int temp=0;
+        int n=arr.size();
+        for(int i=0;i<26;i++)
+        {
+            auto it=upper_bound(arr.begin(),arr.end(),arr[i]+k)-arr.begin();
+            it--;
+            long long sm=p[it]-arr[it];
+            long long req = (n-it-1)*1LL*(arr[i]+k);
+            ans=min(ans,temp+sm-req);
+            temp+=arr[i];
+       
+        }
+        return ans;
+    }
+};
