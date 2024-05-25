fe88d3ed7139c946c12d5081b3352e9c7529ceb0
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-05-25 11:26:56 +0530

    Time: 3 ms (52.46%) | Memory: 7.4 MB (54.43%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,54 +1,21 @@
 class Solution {
 public:
-    void f(int i,int n,vector<int>& ava,string& temp,string& res,string& pattern)
-    {
-        if(i==n)
-        res=min(res,temp);
-        int x=temp.back()-'0';
-        if(pattern[i]=='I')
-        {
-            for(int j=x+1;j<10;j++)
-            {
-                if(ava[j])
-                {
-                    ava[j]--;
-                    temp.push_back(j+'0');
-                    f(i+1,n,ava,temp,res,pattern);
-                    temp.pop_back();
-                    ava[j]++;
-                }
-            }
-        }
-        else
-        {
-            for(int j=x-1;j>=0;j--)
-            {
-                if(ava[j])
-                {
-                    ava[j]--;
-                    temp.push_back(j+'0');
-                    f(i+1,n,ava,temp,res,pattern);
-                    temp.pop_back();
-                    ava[j]++;
-                }
-            }
-        }
-    }
     string smallestNumber(string pattern) {
-        vector<int>ava(10);
-        for(int i=1;i<10;i++)
-        ava[i]++;
-        string temp="";
-        string res="999999999999";
-        int n=pattern.size();
-        for(int i=1;i<10;i++)
+        string result;
+        stack<int> s;
+        for (int i = 0; i <= pattern.length(); i++)
         {
-            temp.push_back(i+'0');
-            ava[i]--;
-            f(0,n,ava,temp,res,pattern);
-            ava[i]++;
-            temp.pop_back();
+            s.push(i + 1);
+            if (i == pattern.length() || pattern[i] == 'I')
+            {
+                while (!s.empty())
+                {
+                    result += to_string(s.top());
+                    s.pop();
+                }
+            }
         }
-        return res;
+        
+        return result;
     }
 };
