2e06064e171ea1da6b783322f62c488838602488
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-06-03 19:46:31 +0530

    Time: 211 ms (82.96%) | Memory: 133 MB (83.60%) - LeetSync

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -19,37 +19,25 @@ public:
     for (int i = 0; i < n; i++)
     {
         p[i].first = a[i];
-        
-        // Storing the original position of a[i]
         p[i].second = i;
     }
-    
     sort(p, p+n);
     int ans = 0;
-    
     for (int i = 0; i < n; i++)
     {	
-    	//visited[i]=true indicates that index i belongs to a cycle that is already counted
-        
-        //p[i].second = i denotes that the ith element was at its correct position
-        
         if (visited[i] || p[i].second == i)
             continue;
             
         int cycle_size = 0;
         int j = i;
-        
-        //Counting the size of the cycle
         while (!visited[j])
         {
             visited[j] = 1;
             j = p[j].second;
             cycle_size++;
         }
-        
         ans += (cycle_size - 1);
     }
-    
     return ans;
     
 }
