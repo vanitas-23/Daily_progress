6c7f7400b11c23c986e5ee17b993b1393b0094ed
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-06-06 10:35:39 +0530

    Time: 1102 ms (5.03%) | Memory: 36 MB (5.06%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int gs) {
+    bool isNStraightHand(vector<int>& hand, int gr) {
+        vector<vector<int>>arr;
         int n=hand.size();
-        if(n%gs!=0)
-        return 0;
-        map<int,int>mp;
-        for(int i:hand)
-        mp[i]++;
-
-       while(!mp.empty())
-       {
-           int curr=mp.begin()->first;
-        //   cout<<curr<<" ";
-           for(int i=0;i<gs;i++)
-           if(mp[curr+i]==0)
-           return 0;
-           else if(mp[curr+i]==1)
-           mp.erase(curr+i);
-           else
-           mp[curr+i]--;
-           
-       }
-       return true;
+        sort(hand.begin(),hand.end());
+        for(int i=0;i<n;i++)
+        {
+            if(arr.size()==0)
+            {
+                vector<int>temp;
+                temp.push_back(hand[i]);
+                arr.push_back(temp);
+                continue;
+            }
+            bool b=0;
+            for(auto& x:arr)
+            {
+                if(x.size()==gr)
+                continue;
+                if(x.back()+1==hand[i])
+                {
+                    //cout<<hand[i]<<" "<<x.back()<<endl;
+                    x.push_back(hand[i]);
+                    
+                    b=1;
+                    break;
+                }
+            }
+            if(!b)
+            {
+                vector<int>temp;
+                temp.push_back(hand[i]);
+                arr.push_back(temp);
+            }
+        }
+        for(auto i:arr)
+        {
+            if(i.size()!=gr)
+            return 0;
+        }
+        return 1;
     }
 };
