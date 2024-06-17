d364ce8e471aca06222b831eb93a2c8d603d8804
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-06-17 19:31:57 +0530

    Time: 70 ms (40.92%) | Memory: 16.6 MB (55.02%) - LeetSync

--- 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-   long long wonderfulSubstrings(string word) {
-    long cnt[1024] = { 1 }, mask = 0, res = 0;
-    for (auto ch : word) {
-        mask ^= 1 << (ch - 'a');
-        res += cnt[mask];
-        for (auto n = 0; n < 10; ++n)
-            res += cnt[mask ^ (1 << n)];
-        ++cnt[mask];
+    long long wonderfulSubstrings(string word) {
+        long mp[1024] = { 1 };
+        int mask=0;
+        long long ans=0;
+        for(auto i:word)
+        {
+            mask^=(1<<(i-'a'));
+            
+            //mp[mask]=1;
+            ans+=mp[mask];
+            for(int j=0;j<10;j++)
+            ans+=mp[mask^(1<<j)];
+            mp[mask]++;
+        }
+        return ans;
     }
-    return res;
-}
 };
