c312d7594880b24116e5666893172068f0dc77f7
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-07-20 08:43:49 +0530

    Time: 1719 ms (20.23%) | Memory: 38.5 MB (64.94%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -3,24 +3,24 @@ public:
     int f(int i,int n,vector<int>& nums,vector<int>& temp,int k)
     {
         if(i==n)
-        return temp.size()>0;
+        {
+            for(int j=0;j<temp.size();j++)
+            {
+                auto it=lower_bound(temp.begin(),temp.end(),temp[j]+k);
+                if(it!=temp.end() && *it==temp[j]+k)
+                return 0;
+            }
+            return 1;
+        }
         int nt=f(i+1,n,nums,temp,k);
-        int t=0;
-        
-        
-            for(int it:temp)
-            if(abs(it-nums[i])==k)
-            return nt;
-            temp.push_back(nums[i]);
-            t=f(i+1,n,nums,temp,k);
-            temp.pop_back();
-        
-        return t+nt;
-        
+        temp.push_back(nums[i]);
+        int t=f(i+1,n,nums,temp,k);
+        temp.pop_back();
+        return nt+t;
     }
     int beautifulSubsets(vector<int>& nums, int k) {
-        int n=nums.size();
+        sort(nums.begin(),nums.end());
         vector<int>temp;
-        return f(0,n,nums,temp,k);
+        return f(0,nums.size(),nums,temp,k)-1;
     }
 };
