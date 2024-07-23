fdec54ebdcce83e3889f8baecd087af932bde818
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-07-23 17:28:36 +0530

    Time: 12 ms (10.56%) | Memory: 15.1 MB (25.59%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i:nums)
+        mp[i]++;
+        auto cmp=[&mp](int a,int b){
+            if(mp[a]==mp[b])
+            return a>b;
+            return mp[a]<mp[b];
+        };
+        sort(nums.begin(),nums.end(),cmp);
+        return nums;
+    }
+};
