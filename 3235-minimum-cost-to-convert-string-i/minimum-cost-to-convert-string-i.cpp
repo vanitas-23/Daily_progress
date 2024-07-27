1edae2e8ca4e1955eade1c3f5a1c26dbcf5d8880
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-07-27 07:34:26 +0530

    Time: 176 ms (60.97%) | Memory: 105.8 MB (39.27%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,46 +1,33 @@
-#define ll long long
-//#define inf 1e10
-#define adj mat
+#include<bits/stdc++.h>
 class Solution {
-public:
-    ll inf=1e12;
+    vector<int>adj[26];
+    //unordered_map<pair<int,int>,int>mp;
     
-    long long minimumCost(string source, string target, vector<char>& original,
-                          vector<char>& changed, vector<int>& cost) {
-        vector<vector<long long>> mat(26, vector<long long>(26, inf));
-       
-        int n = original.size();
-
-        for (int i = 0; i < 26; i++){
-
-            mat[i][i] = 0;
-                          }
-        for (int i = 0; i<n; i++) {
-
-            int start = original[i] - 'a';
-
-            int end = changed[i] - 'a';
-            //cout<<start<<" "<<end<<endl;
-            mat[start][end] = min(mat[start][end], 1LL*cost[i]);
+public:
+     
+   
+    long long minimumCost(string s, string t, vector<char>& org, vector<char>& ch, vector<int>& cost) {
+        vector<vector<long long>>grid(26,vector<long long>(26,1e12));
+        for(int i=0;i<org.size();i++)
+           grid[org[i]-'a'][ch[i]-'a']=min(cost[i]*1ll,grid[org[i]-'a'][ch[i]-'a']);
+        int n=s.size();
+        vector<vector<long long>> temp=grid;
+    for(int i=0;i<26;i++){
+        for(int j=0;j<26;j++){
+            for(int k=0;k<26;k++){
+                temp[j][k]=min(temp[j][i]+temp[i][k],temp[j][k]);
+            }
         }
-        ll ans = 0LL;
-         for (int k = 0; k < 26; k++)
-
-            for (int i = 0; i < 26; i++)
-
-                for (int j = 0; j < 26; j++)
-                    adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
-        
-        for (int i = 0; i < source.size(); i++) {
-            int start = source[i] - 'a';
-            int end = target[i] - 'a';
-            // if (mat[start][end] == inf)
-            //     return -1;
-            if (start == end)
+    }
+         long long ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]==t[i])
                 continue;
-            ans += mat[start][end];
-            if(ans>=inf)
-            return -1;
+            
+            ans+=temp[s[i]-'a'][t[i]-'a'];
+            if(ans>=1e12)
+                return -1;
         }
         return ans;
     }
