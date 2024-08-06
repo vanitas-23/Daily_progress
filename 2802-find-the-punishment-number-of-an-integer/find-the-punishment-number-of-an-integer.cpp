b867af9a3aaa91e61b92d1e572dca0e3fdd339ff
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-08-06 17:32:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (85.06%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,32 +1,24 @@
+bool CanMake(int num, int target) {
+  if (target < 0 || num < target) {
+    return false;
+  }
+  if (num == target) {
+    return true;
+  }
+  return CanMake(num / 10, target - num % 10) || CanMake(num / 100, target - num % 100) ||
+      CanMake(num / 1000, target - num % 1000);
+}
+
 class Solution {
 public:
-    bool f(int i,int n,string s,int org)
-    {
-        if(i==n)
-        return org==0;
-        string curr="";
-        bool b=0;
-        for(int j=i;j<n;j++)
-        {
-            curr+=s[j];
-            b|=f(j+1,n,s,org-stoi(curr));
-        }
-        return b;
+  int punishmentNumber(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n; ++i) {
+      int num = i * i;
+      if (CanMake(num, i)) {
+        answer += num;
+      }
     }
-    int punishmentNumber(int n) {
-        int ans=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(i%9==0 || i%9==1){
-            int x=i*i;
-            string s=to_string(i*i);
-            if(f(0,s.size(),s,i))
-            {
-                // cout<<i<<" ";
-                ans+=x;
-            }
-            }
-        }
-        return ans;
-    }
+    return answer;
+  }
 };
