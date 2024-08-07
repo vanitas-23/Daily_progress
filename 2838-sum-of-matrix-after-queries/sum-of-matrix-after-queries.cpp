9aa5610725eeb90c451f9843634f611c60c4775f
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-08-08 00:14:40 +0530

    Time: 440 ms (35.10%) | Memory: 221 MB (14.90%) - LeetSync

--- 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.cpp
+++ 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.cpp
@@ -10,9 +10,7 @@ public:
             if(queries[i][0]==1)
             {
                 if(col.find(queries[i][1])!=col.end())
-                {
                     continue;
-                }
                 int x=n-row.size();
                 ans+=x*1ll*queries[i][2];
                 col.insert(queries[i][1]);
