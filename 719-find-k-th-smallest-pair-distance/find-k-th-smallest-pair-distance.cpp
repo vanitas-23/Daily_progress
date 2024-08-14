c1b3566641fd6989c3b9c3ff6cec78f72e33aca5
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-08-14 07:14:52 +0530

    Time: 3 ms (98.95%) | Memory: 12.9 MB (25.87%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,23 +1,30 @@
+
 class Solution {
 public:
-    int smallestDistancePair(vector<int>& nums, int k) {
-        const int n=nums.size();
-        vector<int> d(n*(n-1)/2);
-        int index=0;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++)
-                d[index++]=abs(nums[i]-nums[j]);
+    int smallestDistancePair(std::vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        
+        int left = 0;
+        int right = nums[nums.size() - 1] - nums[0];
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            
+            if (issmallpairs(nums, k, mid))
+                right = mid;
+            else
+                left = mid + 1;
         }
-        nth_element(d.begin(), d.begin()+(k-1), d.end());
-        return d[k-1];
+        return left;
+    }
+    
+private:
+    bool issmallpairs(const std::vector<int>& nums, int k, int mid) {
+        int count = 0, left = 0;
+        for (int right = 1; right < nums.size(); right++) {
+            while (nums[right] - nums[left] > mid) left++;
+            count += right - left;
+        }
+        return (count >= k);
     }
 };
-
-
- 
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
