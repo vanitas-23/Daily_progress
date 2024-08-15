1e249952fc9027f1d04ec0b8716fd3f81453efe2
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-08-15 18:07:55 +0530

    Time: 61 ms (41.18%) | Memory: 94.6 MB (36.43%) - LeetSync

--- 2904-sorting-three-groups/sorting-three-groups.cpp
+++ 2904-sorting-three-groups/sorting-three-groups.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int f(int i,int n,vector<int>& a,int pre,vector<vector<int>>& dp)
-    {
-        if(i==n)
-        return 0;
-        if(dp[i][pre]!=-1)
-        return dp[i][pre];
-        int nt=f(i+1,n,a,pre,dp);
-        int t = 0;
-        if(a[i]>=a[pre])
-        t=max(t,1+f(i+1,n,a,i,dp));
-        return dp[i][pre]=max(t,nt);
-    }
-    int minimumOperations(vector<int>& a) {
-        int n=a.size();
-        int ans=0;
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++)
-        ans=max(ans,1+f(i+1,n,a,i,dp));
-        return n-ans;
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int ans = 1;
+        for(int i=1;i<n;i++){
+            int x = upper_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+            if(x==temp.size()) temp.push_back(nums[i]);
+            else{
+                temp[x] = nums[i];
+            }
+        }
+        return n-temp.size();
+        
     }
 };
