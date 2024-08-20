9dd1e52f6c9a008891e2641e9ad99cfa5875e090
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-08-20 11:41:27 +0530

    Time: 447 ms (97.98%) | Memory: 125 MB (56.06%) - LeetSync

--- 2953-count-pairs-of-points-with-distance-k/count-pairs-of-points-with-distance-k.cpp
+++ 2953-count-pairs-of-points-with-distance-k/count-pairs-of-points-with-distance-k.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
+    long long hash(int x, int y) {
+        return ((long long)x << 32) + y;
+    }
+    pair<int,int> getHash(long long hashValue) {
+        return {hashValue >> 32, hashValue & (0xffffffff)}; 
+    }
     int countPairs(vector<vector<int>>& coordinates, int k) {
-        set<pair<int,int>> stp ;
-        for(int i = 0 ; i <=k ; i++){
-            stp.insert({i,k-i}) ;
-            stp.insert({k-i,i}) ;
+        unordered_map<long long, int> freq;
+
+        for (const auto &coordinate : coordinates) {
+            freq[hash(coordinate[0], coordinate[1])] ++;
         }
-        
-        map<pair<int,int>,int> mp ;
-        int ans = 0 ;
-        for(auto point : coordinates){
-            int x = point[0] , y = point[1] ;
-            for(auto prev : stp){
-                int first = prev.first^x , second = prev.second^y ;
-                if(mp.find({first , second}) != mp.end()){
-                    ans += mp[{first,second}] ;
+
+
+        long long res = 0;
+        for (int i = 0; i <= k; i ++) {
+            int disX = i;
+            int disY = k - i;
+            for (const auto &coordinate : coordinates) {
+                int matchX = disX ^ coordinate[0];
+                int matchY = disY ^ coordinate[1];
+                auto it = freq.find(hash(matchX, matchY));
+                if (it != freq.end()) {
+                    if (matchX == coordinate[0] && matchY == coordinate[1]) {
+                        res += it->second - 1 ;
+                    } else {
+                        res += it->second;
+                    }
                 }
             }
-            mp[{x,y}]++;
         }
-        return ans ;
+        return res / 2;
     }
 };
