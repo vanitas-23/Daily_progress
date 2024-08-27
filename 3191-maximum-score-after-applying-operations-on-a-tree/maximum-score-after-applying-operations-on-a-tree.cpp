bac83ad721bd171cbb78c50e64b5ef2ba345117a
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-08-27 17:56:01 +0530

    Time: 336 ms (22.08%) | Memory: 170.3 MB (29.19%) - LeetSync

---
+++ 3191-maximum-score-after-applying-operations-on-a-tree/maximum-score-after-applying-operations-on-a-tree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> adj;
+    
+    const long long INF = 1e18+10;
+    long long dfs(int c, int p, const vector<int>& a) {
+        long long ans = a[c];
+        long long x = 0; bool b = 0;
+        for (int nxt : adj[c]) if (nxt != p) {
+            x += dfs(nxt, c, a);
+            b = 1;
+        }
+        if (!b) return ans;
+        return min(ans, x);
+    }
+    long long maximumScoreAfterOperations(vector<vector<int>>& edges, vector<int>& values) {
+        int n = values.size();
+        adj.clear(); adj.resize(n);
+        for (auto v : edges) {
+            int a = v[0], b = v[1];
+            adj[a].push_back(b), adj[b].push_back(a);
+        }
+        
+        long long ans = accumulate(values.begin(),values.end(),0LL);
+        
+        return ans - dfs(0, -1, values);
+    }
+};
