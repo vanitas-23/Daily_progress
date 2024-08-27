6f128b53ab89a1a875d00e6736b8dcd53651bd03
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-08-27 17:57:16 +0530

    Time: 323 ms (27.16%) | Memory: 179.1 MB (15.23%) - LeetSync

--- 3191-maximum-score-after-applying-operations-on-a-tree/maximum-score-after-applying-operations-on-a-tree.cpp
+++ 3191-maximum-score-after-applying-operations-on-a-tree/maximum-score-after-applying-operations-on-a-tree.cpp
@@ -1,28 +1,43 @@
+#define ll long long
 class Solution {
 public:
-    vector<vector<int>> adj;
-    
-    const long long INF = 1e18+10;
-    long long dfs(int c, int p, const vector<int>& a) {
-        long long ans = a[c];
-        long long x = 0; bool b = 0;
-        for (int nxt : adj[c]) if (nxt != p) {
-            x += dfs(nxt, c, a);
-            b = 1;
+    long long f(int node,int par,int istaken,vector<int>* adj,vector<int>& values,vector<vector<ll>>& dp)
+    {
+        if(adj[node].size()==1 && adj[node][0]==par)
+        {
+            if(istaken)
+            return values[node];
+            return 0;
         }
-        if (!b) return ans;
-        return min(ans, x);
+        if(dp[node][istaken]!=-1)
+        return dp[node][istaken];
+        if(istaken)
+        {
+            ll val=values[node];
+            for(auto i:adj[node])
+            if(i!=par)
+            val+=f(i,node,istaken,adj,values,dp);
+            return dp[node][istaken]=val;
+        }
+        ll case1=0;
+        ll case2=values[node];
+        for(auto i:adj[node])
+        if(i!=par)
+        case1+=f(i,node,1,adj,values,dp);
+        for(auto i:adj[node])
+        if(i!=par)
+        case2+=f(i,node,0,adj,values,dp);
+        return dp[node][istaken]=max(case1,case2);
     }
     long long maximumScoreAfterOperations(vector<vector<int>>& edges, vector<int>& values) {
-        int n = values.size();
-        adj.clear(); adj.resize(n);
-        for (auto v : edges) {
-            int a = v[0], b = v[1];
-            adj[a].push_back(b), adj[b].push_back(a);
+        int n=values.size();
+        vector<int>adj[n];
+        for(auto i:edges)
+        {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
         }
-        
-        long long ans = accumulate(values.begin(),values.end(),0LL);
-        
-        return ans - dfs(0, -1, values);
+        vector<vector<ll>>dp(n,vector<ll>(2,-1));
+        return f(0,-1,0,adj,values,dp);
     }
 };
