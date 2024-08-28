7a330699c4d596923169958f4930e1a47e688879
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-08-28 18:32:21 +0530

    Time: 629 ms (16.44%) | Memory: 147.2 MB (22.87%) - LeetSync

---
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool dfs(int i,int j,int n,int m,vector<vector<int>>& grid1, vector<vector<int>>& grid2)
+    {
+        if(grid1[i][j]!=1)
+        return 0;
+        vector<int>dx={-1,1,0,0};
+        vector<int>dy={0,0,1,-1};
+        grid2[i][j]=0; bool x=1;
+        for(int k=0;k<4;k++)
+        {
+            int r=i+dx[k];
+            int c=j+dy[k];
+            if(r>=0 && r<n && c>=0 && c<m && grid2[r][c])
+            x&=dfs(r,c,n,m,grid1,grid2);
+        }
+        return x;
+    }
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int n=grid1.size();
+        int m=grid1[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+        {
+            if(grid2[i][j])
+            ans+=dfs(i,j,n,m,grid1,grid2);
+        }
+        return ans;
+    }
+};
