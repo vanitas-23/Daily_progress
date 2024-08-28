85ddc8ff4e2f337ba5317878e1f3d35969d979e4
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-08-28 18:46:02 +0530

    Time: 430 ms (20.67%) | Memory: 45.2 MB (11.73%) - LeetSync

--- 3200-number-of-strings-which-can-be-rearranged-to-contain-substring/number-of-strings-which-can-be-rearranged-to-contain-substring.cpp
+++ 3200-number-of-strings-which-can-be-rearranged-to-contain-substring/number-of-strings-which-can-be-rearranged-to-contain-substring.cpp
@@ -1,34 +1,36 @@
-typedef long long int ll;
-
-const int MOD = 1e9+7;
-
 class Solution {
-    
-    ll FastPower (ll a, ll b) {
-        ll ans = 1;
-        while (b) {
-            if (b&1) ans = (ans * a) % MOD;
-            a = (a * a) % MOD;
-            b /= 2;
-        }
-        return ans;
-    }
-    
 public:
+    int m=1e9+7;
+    int f(int i,int n,int mask,vector<vector<int>>& dp)
+    {
+        if(i==n)
+        return __builtin_popcount(mask)==4;
+        if(dp[i][mask]!=-1)
+        return dp[i][mask];
+        int ans=0;
+        for(int j=0;j<26;j++)
+        {
+            if(j==4)
+            {
+                if(mask&1)
+                ans=(ans%m+f(i+1,n,mask|(2),dp)%m)%m;
+                else
+                ans=(ans%m+f(i+1,n,mask|(1),dp)%m)%m;
+            }
+            else if(j==11)
+            ans=(ans%m+f(i+1,n,mask|(4),dp)%m)%m;
+            else if(j==19)
+            ans=(ans%m+f(i+1,n,mask|(8),dp)%m)%m;
+            else
+            ans=(ans%m+f(i+1,n,mask,dp)%m)%m;
+        }
+        return dp[i][mask]=ans;
+    }
     int stringCount(int n) {
-        return (
-            FastPower(26, n)
-            -
-            (
-                (FastPower(25, n-1) * (n + 75)) % MOD 
-                -
-                (FastPower(24, n-1) * (n*2 + 72)) % MOD
-                + 
-                (FastPower(23, n-1) * (n + 23)) % MOD
-                + MOD
-            ) % MOD
-            + 
-            MOD
-        ) % MOD;
+        if(n<4)
+        return 0;
+        int mask=0;
+        vector<vector<int>>dp(n,vector<int>(16,-1));
+        return f(0,n,mask,dp);
     }
 };
