bfe97a945bf1509c6e460141a612b7883fe75199
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-01 12:34:15 +0530

    Time: 312 ms (63.93%) | Memory: 166.1 MB (68.57%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        int n = nums.size();
-        vector<pair<int, int>> nums2(n);
-        for (int i = 0; i < n; i++) {
-            nums2[i] = {nums[i], i};
-        }
-        sort(nums2.begin(), nums2.end());
-        vector<vector<int>> indices;
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || nums2[i].first - nums2[i - 1].first > limit) {
-                indices.push_back({});
+        vector<pair<int,int>>temp;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        temp.push_back({nums[i],i});
+        sort(temp.begin(),temp.end());
+        vector<int>ans(n);
+        int curr=-1;
+        int last=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(curr==-1)
+            curr=temp[i].first,last=i;
+            else
+            {
+                if(temp[i].first-curr>limit)
+                {
+                    vector<int>idc;
+                    vector<int>ele;
+                    for(int j=last;j<i;j++)
+                    idc.push_back(temp[j].second),ele.push_back(temp[j].first);
+                    sort(idc.begin(),idc.end());
+                    sort(ele.begin(),ele.end());
+                    for(int j=0;j<idc.size();j++)
+                    ans[idc[j]]=ele[j];
+                    last=i;
+                }
+                curr=temp[i].first;
             }
-            indices.back().push_back(nums2[i].second);
         }
-        vector<int> result(n);
-        for (auto& index: indices) {
-            vector<int> sortedIndex(index);
-            sort(sortedIndex.begin(), sortedIndex.end());
-            for (int j = 0; j < index.size(); j++) {
-                result[sortedIndex[j]] = nums[index[j]];
-            }
-        }
-        return result;
+        vector<int>idc;
+                    vector<int>ele;
+                    for(int j=last;j<n;j++)
+                    idc.push_back(temp[j].second),ele.push_back(temp[j].first);
+                    sort(idc.begin(),idc.end());
+                    sort(ele.begin(),ele.end());
+                    for(int j=0;j<idc.size();j++)
+                    ans[idc[j]]=ele[j];
+        return ans;
     }
 };
