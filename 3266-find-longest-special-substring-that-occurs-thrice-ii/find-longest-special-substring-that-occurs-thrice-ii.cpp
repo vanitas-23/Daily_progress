4ca1742aa7188caad996c65080f85461725125df
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-05 00:32:28 +0530

    Time: 706 ms (16.04%) | Memory: 37.9 MB (84.63%) - LeetSync

--- 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.cpp
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
-    int maximumLength(string s) {
-        vector<vector<int>>req(26);
-
-        int n=s.size();
-        int i=0;
-        int j=1;
-        while(j<n){
-            if(s[j-1]!=s[j]){
-            req[s[j-1]-'a'].push_back(j-i);
-            i=j;
-        }
-        j++;
-    }
-    req[s[j-1]-'a'].push_back(j-i);
-    int mx=-1;
-
-    for(int i=0;i<26;i++){
-        if(req[i].size()>=3)
+    bool f(string& s,int k)
+    {
+        unordered_map<int,int>mp;
+        vector<int>arr(26);
+        int mask=0;
+        for(int i=0;i<s.size();i++)
         {
-
-        sort(req[i].begin(),req[i].end());
-
-        mx=max(mx,req[i][req[i].size()-3]);
-        mx=max(mx,req[i][req[i].size()-1]-2);
-        if(req[i][req[i].size()-1]==req[i][req[i].size()-2])
-        mx=max(mx,req[i][req[i].size()-1]-1);
+            arr[s[i]-'a']++;
+            mask|=(1<<(s[i]-'a'));
+            if(i<k-1)
+            continue;
+            
+            if(__builtin_popcount(mask)==1)
+            mp[mask]++;
+            arr[s[i-k+1]-'a']--;
+            if(arr[s[i-k+1]-'a']==0)
+            mask^=(1<<(s[i-k+1]-'a'));
         }
-        else if(req[i].size()==2)
+        
+        for(auto i:mp)
+        if(i.second>=3)
+        return 1;
+        return 0;
+    }
+    int maximumLength(string s) {
+        
+        int n=s.size();
+        int l=1;
+        int r=n;
+        int ans=0;
+        while(l<=r)
         {
             
-            int sb=max(req[i][0],req[i][1]);
-            if(req[i][0]+req[i][1]<3)
-            continue;
-            if(req[i][0]==req[i][1]){
-                mx=max(mx,req[i][0]-1);
-                continue;
+            int mid=(l+r)/2;
+            cout<<mid<<" ";
+            if(f(s,mid))
+            {
+                ans=mid;
+                l=mid+1;
             }
-            sb-=2;
-
-            sb=max(sb,min(req[i][0],req[i][1]));
-            mx=max(mx,sb);
+            else
+            r=mid-1;
         }
-        else if(req[i].size()==1)
-        mx=max(mx,req[i][0]-2);
-    }
-    return mx==0?-1:mx;
+        return ans==0?-1:ans;
     }
 };
