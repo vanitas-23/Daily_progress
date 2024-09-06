da8bc91a42e98dea96d3d44efdf10cae876a4bee
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-06 17:48:33 +0530

    Time: 700 ms (7.36%) | Memory: 42.5 MB (34.52%) - LeetSync

--- 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/count-pairs-of-connectable-servers-in-a-weighted-tree-network.cpp
+++ 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/count-pairs-of-connectable-servers-in-a-weighted-tree-network.cpp
@@ -1,30 +1,41 @@
-#define ll long long
 class Solution {
 public:
-    
-   int dfs(int i, int p, int s, int ss, vector<vector<pair<int, int>>> &al) {
-    int res = s % ss == 0;
-    for (auto [j, w] : al[i])
-        if (j != p)
-            res += dfs(j, i, s + w, ss, al);
-    return res;
-}
-vector<int> countPairsOfConnectableServers(vector<vector<int>>& edges, int signalSpeed) {
-    int n = edges.size() + 1;
-    vector<vector<pair<int, int>>> al(n);
-    vector<int> res(n);
-    for (const auto &e : edges) {
-        al[e[0]].push_back({e[1], e[2]});
-        al[e[1]].push_back({e[0], e[2]});
+    map<pair<int,int>,int>mp;
+    int f(int node,vector<int>adj[],int par,int curr,int ss)
+    {
+        int ans=(curr%ss)==0;
+        for(auto i:adj[node])
+        if(i!=par)
+        ans+=f(i,adj,node,curr+mp[{node,i}],ss);
+        return ans;
     }
-    for (int i = 0; i < n; ++i) {
-        int cnt = 0;
-        for (auto [j, w] : al[i]) {
-            int cnt_j = dfs(j, i, w, signalSpeed, al);
-            res[i] += cnt * cnt_j;
-            cnt += cnt_j;
-        }            
-    }            
-    return res;
-}
+    vector<int> countPairsOfConnectableServers(vector<vector<int>>& edges, int ss) {
+        int n=edges.size();
+        vector<int>adj[n+1];
+
+        for(auto i:edges)
+        {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+            mp[{i[0],i[1]}]=i[2];
+            mp[{i[1],i[0]}]=i[2];
+        }
+        vector<int>ans(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            vector<int>x;
+            for(auto j:adj[i])
+            x.push_back(f(j,adj,i,mp[{i,j}],ss));
+            if(x.size()==1)
+            continue;
+            int req=0;
+            auto temp=x;
+            for(int j=temp.size()-2;j>=0;j--)
+            temp[j]+=temp[j+1];
+            for(int j=0;j<x.size()-1;j++)
+            req+=(x[j]*temp[j+1]);
+            ans[i]=req;
+        }
+        return ans;
+    }
 };
