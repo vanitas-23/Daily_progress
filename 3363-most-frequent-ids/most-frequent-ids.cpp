50d707bdb219700b41a6d445a201848b5ee201ce
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-10 23:32:26 +0530

    Time: 564 ms (8.99%) | Memory: 204.8 MB (6.18%) - LeetSync

---
+++ 3363-most-frequent-ids/most-frequent-ids.cpp
@@ -0,0 +1,27 @@
+#define ll long long
+class Solution {
+public:
+    vector<long long> mostFrequentIDs(vector<int>& nums, vector<int>& frq) {
+        int n=nums.size();
+        multiset<ll>st;
+        map<int,ll>mp;
+        vector<ll>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(mp.find(nums[i])==mp.end())
+            {
+                mp[nums[i]]+=frq[i];
+                st.insert(frq[i]);
+                ans.push_back(*st.rbegin());
+                continue;
+            }
+            ll prv=mp[nums[i]];
+            auto it=st.lower_bound(prv);
+            st.erase(it);
+            mp[nums[i]]+=frq[i];
+            st.insert(mp[nums[i]]);
+            ans.push_back(*st.rbegin());
+        }
+        return ans;
+    }
+};
