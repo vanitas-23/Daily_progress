fb701a2dc94594a4c1fa6a5202868bcf5bbbbd0c
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-12 10:09:57 +0530

    Time: 2 ms (57.27%) | Memory: 9.2 MB (28.19%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    long long minEnd(long long n, long long x) {
+    long long minEnd(int n, int x) {
         vector<int>arr;
-        for(int i=0;i<64;i++)
-        {
-            if(x&(1LL<<i))
-            continue;
-            arr.push_back(i);
+        for(int i=0;i<64;i++){
+        if(x&(1ll<<i))
+        continue;
+        arr.push_back(i);
         }
-        int ans=n-1;
+        n--;
         int j=0;
+        long long ans=x;
+        //cout<<ans<<" "<<n;
         for(int i=0;i<64;i++)
         {
-            if(ans&(1LL<<i))
-            x|=(1LL<<arr[j]);
+            if((n*1ll)&(1ll<<i))
+            ans|=(1ll<<arr[j]);
             j++;
         }
-        return x;
+        return ans;
     }
 };
