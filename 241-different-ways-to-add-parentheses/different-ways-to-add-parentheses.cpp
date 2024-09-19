c65f8c54575aecbb69eeccd636a1203277abacc0
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-19 09:23:51 +0530

    Time: 6 ms (19.82%) | Memory: 14.6 MB (6.55%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> f(int i,int n,string exp)
+    {
+        if(i==n-1)
+        return {exp[i]-'0'};
+        string temp="";
+        vector<int>arr;
+        bool op=0;
+        for(int j=i;j<n;j++)
+        {
+            if(exp[j]=='+' || exp[j]=='-' || exp[j]=='*')
+            {
+                op=1;
+                auto x=f(i,j,exp);
+                auto y=f(j+1,n,exp);
+                for(auto l:x)
+                for(auto r:y)
+                {
+                    if(exp[j]=='+')
+                    arr.push_back(l+r);
+                    else if(exp[j]=='-')
+                    arr.push_back(l-r);
+                    else
+                    arr.push_back(l*r);
+                }
+            }
+        }
+        if(op==0)
+        arr.push_back(stoi(exp.substr(i,n-i)));
+        return arr;
+    }
+    vector<int> diffWaysToCompute(string exp) {
+        int n=exp.size();
+        return f(0,n,exp);
+    }
+};
