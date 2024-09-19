e511a9684dad6c4d289ef7572c8aeb2378781dd6
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-19 09:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (81.44%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    vector<int> f(int i,int n,string exp)
+    vector<int> f(int i,int n,string exp,vector<vector<vector<int>>>& dp)
     {
-        if(i==n-1)
-        return {exp[i]-'0'};
+        if(dp[i][n].size()!=0)
+        return dp[i][n];
         vector<int>arr;
         bool op=0;
+    
         for(int j=i;j<n;j++)
         {
             if(exp[j]=='+' || exp[j]=='-' || exp[j]=='*')
             {
                 op=1;
-                auto x=f(i,j,exp);
-                auto y=f(j+1,n,exp);
+                auto x=f(i,j,exp,dp);
+                auto y=f(j+1,n,exp,dp);
                 for(auto l:x)
                 for(auto r:y)
                 {
@@ -27,10 +28,11 @@ public:
         }
         if(op==0)
         arr.push_back(stoi(exp.substr(i,n-i)));
-        return arr;
+        return dp[i][n]=arr;
     }
     vector<int> diffWaysToCompute(string exp) {
         int n=exp.size();
-        return f(0,n,exp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1));
+        return f(0,n,exp,dp);
     }
 };
