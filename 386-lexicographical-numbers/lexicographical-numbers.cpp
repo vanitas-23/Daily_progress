4a5a98bdf7234e5a2050d967fad4485eaa88d746
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-21 17:57:00 +0530

    Time: 8 ms (73.38%) | Memory: 14.4 MB (42.87%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+    vector<int>dig={0,1,2,3,4,5,6,7,8,9};
+    vector<int>arr;
+    void f(int i,int n)
+    {
+        for(auto j:dig)
+        {
+            if(i*10+j==0)
+            continue;
+            if(i*10+j>n)
+            continue;
+            arr.push_back(i*10+j);
+            f(i*10+j,n);
+        }
+    }
     vector<int> lexicalOrder(int n) {
-        vector<int>arr(n);
-        iota(arr.begin(),arr.end(),1);
-        auto cmp=[](int a,int b){
-            string s=to_string(a);
-            string t=to_string(b);
-            return s<t;
-        };
-        sort(arr.begin(),arr.end(),cmp);
+        f(0,n);
         return arr;
     }
 };
