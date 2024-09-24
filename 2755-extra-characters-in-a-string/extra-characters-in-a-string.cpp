a99ab3df887f0acc02b3fcf8e9f12969bb739d78
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-24 07:38:17 +0530

    Time: 408 ms (10.56%) | Memory: 145.7 MB (12.26%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,32 +1,31 @@
+
 class Solution {
 public:
-    int solve(string& s, unordered_map<string, int>&mp, vector<int>&dp, int index)
+    map<string,int>mp;
+    int f(int i,int n,string s,vector<int>& dp)
     {
-        if (index >= s.size()) return 0;
-        if (dp[index] != -1) return dp[index]; 
-        
-        string currStr = "";
-        int minExtra = s.size();
-        for (int cutIdx = index; cutIdx < s.size(); cutIdx++)
+        if(i==n)
+        return 0;
+        if(dp[i]!=-1)
+        return dp[i];
+        int nt=1+f(i+1,n,s,dp);
+        string temp="";
+        for(int j=i;j<n;j++)
         {
-            currStr.push_back(s[cutIdx]);
-    
-            int currExtra = (mp.count(currStr))? 0 : currStr.size();
-            int nextExtra = solve(s, mp, dp, cutIdx + 1);
-            int totalExtra = currExtra + nextExtra;
-            
-            minExtra = min(minExtra, totalExtra);
+            temp+=s[j];
+            if(mp.find(temp)!=mp.end())
+            nt=min(nt,f(j+1,n,s,dp));
+            else
+            nt=min(nt,j-i+1+f(j+1,n,s,dp));
         }
-        return dp[index] = minExtra;
+        return dp[i]=nt;
     }
-    int minExtraChar(string s, vector<string>& dictionary) 
-    {
-        vector<int>dp(s.size(), -1);
-        unordered_map<string, int>mp;
-        for (string& word : dictionary) mp[word]++;
+    int minExtraChar(string s, vector<string>& dic) {
         
-        int ans = solve(s, mp, dp, 0);
-        return ans;
-        
+        for(auto i:dic)
+        mp[i]++;
+        int n=s.size();
+        vector<int>dp(n,-1);
+        return f(0,n,s,dp);
     }
 };
