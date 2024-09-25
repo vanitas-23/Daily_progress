462ef851412159515fb5af6eaac73223f53847b1
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-09-25 19:41:21 +0530

    Time: 1359 ms (5.23%) | Memory: 733.4 MB (14.21%) - LeetSync

---
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,50 @@
+class TrieNode {
+public:
+    unordered_map<char, pair<int,TrieNode*>> children;
+    bool isEndOfWord = false;
+};
+
+class Trie {
+public:
+    TrieNode* root = new TrieNode();
+    
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children.find(c) == node->children.end()) {
+                node->children[c].second = new TrieNode();
+                node->children[c].first=1;
+            }
+            else
+            node->children[c].first++;
+            node = node->children[c].second;
+        }
+        node->isEndOfWord = true;
+    }
+    int length(string s)
+    {
+        int ans=0;
+        auto node=root;
+        for(char c:s)
+        {
+            if(node->children.find(c)==node->children.end())
+            return ans;
+            ans+=node->children[c].first;
+            node=node->children[c].second;
+        }
+        return ans;
+    }
+    
+};
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        Trie tr;
+        for(auto i:words)
+        tr.insert(i);
+        vector<int>ans;
+        for(auto i:words)
+        ans.push_back(tr.length(i));
+        return ans;
+    }
+};
