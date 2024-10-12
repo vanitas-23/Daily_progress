feacbe6d71d39984ab0d372310e666f654ff8c42
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-10-12 08:40:07 +0530

    Time: 368 ms (11.94%) | Memory: 94.9 MB (9.21%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int smallestChair(vector<vector<int>>& times, int targetFriend) {
-        vector<int> targetTime = times[targetFriend];
-        sort(times.begin(), times.end());
+    int smallestChair(vector<vector<int>>& times, int t) {
+        auto x = times;
+        sort(x.begin(), x.end());
 
         int n = times.size();
-        vector<int> chairTime(n);
-
-        for (auto time : times) {
-            for (int i = 0; i < n; i++) {
-                if (chairTime[i] <= time[0]) {
-                    chairTime[i] = time[1];
-                    if (time == targetTime) return i;
-                    break;
-                }
+        int lst=x.back()[1];
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<n;i++)
+        pq.push(i);
+        unordered_map<int,pair<int,int>>arr;
+        map<int,vector<int>>dep;
+        for(int i=0;i<n;i++)
+        arr[times[i][0]]={times[i][1],i};
+        vector<int>ans(n);
+        for(int i=1;i<=lst;i++)
+        {
+            if(dep.find(i)!=dep.end())
+            {
+                for(auto j:dep[i])
+                pq.push(j);
             }
+            if(arr.find(i)!=arr.end())
+            {
+                ans[arr[i].second]=pq.top();
+                dep[arr[i].first].push_back(pq.top());
+                pq.pop();;
+            }
+            
         }
-        return 0;
+        return ans[t];
     }
 };
