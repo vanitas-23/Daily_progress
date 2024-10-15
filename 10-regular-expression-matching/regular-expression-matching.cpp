839b7db758e9177cec378d55495fd2976b467332
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-10-15 19:29:10 +0530

    Time: 14 ms (17.70%) | Memory: 22.9 MB (5.12%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+ int t[21][21];
+ bool solve(int i , int j , string s , string p){
+     if(j == p.length()){
+        return i == s.length();
+     }
+     if(t[i][j]!= -1) return t[i][j];
+
+     bool first_char = false;
+     if(i < s.length() && (p[j] == s[i] || p[j] == '.'))
+      first_char = true;
+ 
+ if(j+1 < p.length() && p[j+1] == '*'){
+      bool not_take = solve(i, j+2 , s, p);
+      bool take = first_char && solve(i+1 , j , s, p);
+      return t[i][j] = not_take || take;
+ }
+ return t[i][j] = first_char && solve(i+1,j+1 , s ,p);
+ }
+    bool isMatch(string s, string p) {
+        memset(t,-1,sizeof(t));
+        return solve(0,0,s,p);
+    }
+};
