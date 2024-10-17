037a8700648a57e233509369546b147ff63d0b27
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-10-17 14:27:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (42.53%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int f(int i,int n,int step,vector<int>& nums,vector<vector<int>>& dp)
-    {
-        if(i==n-1)
-        return 0;
-        if(dp[i][step]!=-1)
-        return dp[i][step];
-        int taken = 1+f(i+1,n,nums[i],nums,dp);
-        int ntaken=INT_MAX;
-        if(step>1)
-        ntaken=f(i+1,n,step-1,nums,dp);
-        return dp[i][step]=min(taken,ntaken);
-    }
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)
-        return 0;
-        vector<vector<int>>dp(n-1,vector<int>(*max_element(nums.begin(),nums.end())+1,-1));
-        return 1+f(1,n,nums[0],nums,dp);
+        int jumps=0,l=0,r=0;
+        while(r<nums.size()-1){
+            int farthest=0;
+            for(int i=l;i<=r;i++){
+                farthest=max(farthest,i+nums[i]);
+            }
+            l=r+1;
+            r=farthest;
+            jumps++;
+        }
+        return jumps;
     }
 };
