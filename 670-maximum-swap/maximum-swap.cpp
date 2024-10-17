49b8245aa13acf6a50b1381599c49081eef7af30
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-10-17 09:09:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (89.76%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        string s = to_string(num);
-        string t = s;
-
-        sort(s.rbegin(), s.rend());
-
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] != t[i]) {
-                int index = -1;
-                for (int j = i + 1; j < t.size(); j++) {
-                    if (t[j] == s[i]) {
-                        index = j;
-                    }
+        
+        string s=to_string(num);
+        for(int i=0;i<s.size();i++)
+        {
+            int curr=s[i]-'0';
+            int id=i;
+            int mx=curr;
+            for(int j=i+1;j<s.size();j++)
+            {
+                if(s[j]-'0'>=mx)
+                {
+                    mx=s[j]-'0';
+                    id=j;
                 }
-                swap(t[index], t[i]);
+            }
+            //cout<<i<<" "<<id<<endl;
+            if(id!=i && mx!=curr)
+            {
+                swap(s[i],s[id]);
                 break;
             }
         }
-
-        return stoi(t);
+        return stoi(s);
     }
 };
