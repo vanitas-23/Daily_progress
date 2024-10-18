c33cc917ea14ea97dbcbd57ceff9e9a867f0f578
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-10-18 11:44:58 +0530

    Time: 3 ms (99.75%) | Memory: 10.1 MB (82.14%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int f(int i,int n,vector<int>& nums,int curr,int mx, vector<vector<int>>& dp)
-    {
-        if(i==n)
-        return curr==mx;
-        if(dp[i][curr]!=-1)
-        return dp[i][curr];
-        int nt=f(i+1,n,nums,curr,mx,dp);
-        int t=f(i+1,n,nums,curr|nums[i],mx,dp);
-        return dp[i][curr]=t+nt;
-    }
+    int max,count;
     int countMaxOrSubsets(vector<int>& nums) {
-        int mx=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-            mx|=nums[i];
-        vector<vector<int>>dp(n,vector<int>(mx+1,-1));
-        return f(0,n,nums,0,mx,dp);
+        max = 0,count = 0;
+
+        int target = 0;
+        for(auto &s:nums){
+            target |= s;
+        }
+
+        solve(nums,0,0,target);
+        return count;
+    }
+    void solve(vector<int>&nums,int pos,int result,int target){
+        if(result == target){
+            count += 1 << (nums.size()-pos);
+            return;
+        }
+
+        if(pos == nums.size()) return;
+
+        solve(nums,pos+1,result | nums[pos],target);
+
+        solve(nums,pos+1,result,target);
     }
 };
