efba917f1a2c6536eb0638e8fbd030ed6087c16e
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-10-19 15:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (64.16%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -3,12 +3,12 @@ public:
     char findKthBit(int n, int k) {
         if(n==1)
         return '0';
-        int len=(1<<n)-1;
-        int mid=(len+1)/2;
+        int len=(1<<n);
+        int mid=len/2;
         if(k==mid)
         return '1';
         if(k<mid)
         return findKthBit(n-1,k);
-        return findKthBit(n-1,len+1-k)=='0'?'1':'0';
+        return findKthBit(n-1,len-k)=='0'?'1':'0';
     }
 };
