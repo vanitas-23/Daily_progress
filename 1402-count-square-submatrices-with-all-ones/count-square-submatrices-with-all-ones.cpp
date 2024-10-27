bbd4280ace2854c62951ff997018e1ae6c61c7d4
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-10-27 17:57:53 +0530

    Time: 11 ms (25.93%) | Memory: 30.1 MB (5.31%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& grid,
-              vector<vector<int>>& dp) {
-        // If the cell lies outside the grid, return 0.
-        if (i >= grid.size() || j >= grid[0].size()) return 0;
-        if (grid[i][j] == 0) return 0;
-
-        // If we have already visited this cell, return the memoized value.
-        if (dp[i][j] != -1) return dp[i][j];
-
-        // Find the answer for the cell to the right of the current cell.
-        int right = solve(i, j + 1, grid, dp);
-        // Find the answer for the cell to the diagonal of the current cell.
-        int diagonal = solve(i + 1, j + 1, grid, dp);
-        // Find the answer for the cell below the current cell.
-        int below = solve(i + 1, j, grid, dp);
-
-        return dp[i][j] = 1 + min(right, min(diagonal, below));
+    int f(int i,int j,int n,int m,vector<vector<int>>& mat,vector<vector<int>>& dp)
+    {
+        if(i==n || j==m)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        if(mat[i][j]==0)
+        return dp[i][j]=0;
+        return dp[i][j]=1+min({f(i+1,j,n,m,mat,dp),f(i,j+1,n,m,mat,dp),f(i+1,j+1,n,m,mat,dp)});
     }
-
-    int countSquares(vector<vector<int>>& grid) {
-        int ans = 0;
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                ans += solve(i, j, grid, dp);
-            }
-        }
+    int countSquares(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int ans=0;
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+        if(mat[i][j])
+        ans+=f(i,j,n,m,mat,dp);
         return ans;
     }
 };
