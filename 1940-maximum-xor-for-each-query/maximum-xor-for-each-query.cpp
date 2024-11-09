096b980520c7d2f6b75f65e31ec8978141bc75f6
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2024-11-09 10:28:31 +0530

    Time: 17 ms (12.38%) | Memory: 99.8 MB (27.91%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
@@ -4,17 +4,12 @@ public:
         vector<int>arr;
         int n=nums.size();
         int curr=0;
+        int mask=(1<<maximumBit)-1;
         for(int i:nums)
         curr^=i;
         for(int i=0;i<n;i++)
         {
-            int ans=0;
-            for(int j=0;j<maximumBit;j++){
-            if(curr&(1<<j))
-            continue;
-            ans|=(1<<j);
-            }
-            arr.push_back(ans);
+            arr.push_back(curr^mask);
             curr^=nums[n-1-i];
         }
         return arr;
