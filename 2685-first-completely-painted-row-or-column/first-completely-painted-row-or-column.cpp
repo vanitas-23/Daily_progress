b8f1e72926f8cc6be77e0bf9086c802af55df212
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-01-20 20:06:44 +0530

    Time: 107 ms (42.07%) | Memory: 170.3 MB (56.71%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -3,7 +3,7 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        map<int,pair<int,int>>mp;
+        unordered_map<int,pair<int,int>>mp;
         for(int i=0;i<n;i++)
         for(int j=0;j<m;j++)
         mp[mat[i][j]]={i,j};
