d67ad4ece5be2f1ce9008fb4df739dd242b00f93
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-01-27 13:58:34 +0530

    Time: 52 ms (56.47%) | Memory: 69.5 MB (61.65%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pr, vector<vector<int>>& q) {
+        vector<vector<int>>mat(n,vector<int>(n,0));
+        for(int i=0;i<pr.size();i++)
+        mat[pr[i][0]][pr[i][1]]=1;
+        for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+        for(int k=0;k<n;k++)
+        if(mat[j][i] && mat[i][k]) mat[j][k]=1;
+
+        
+        vector<bool>ans;
+        for(auto i:q) ans.push_back(mat[i[0]][i[1]]);
+        return ans;
+    }
+};
