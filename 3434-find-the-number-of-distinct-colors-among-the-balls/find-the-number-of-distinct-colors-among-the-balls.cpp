ca61e220e8b9fa74b7994b30867a4ff617a2f430
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-02-07 21:15:48 +0530

    Time: 290 ms (10.94%) | Memory: 174.7 MB (13.51%) - LeetSync

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        map<int,int>coCount;
+        map<int,int>co;
+        vector<int>ans;
+        for(auto i:queries)
+        {
+            if(co.find(i[0])==co.end())
+            {
+                co[i[0]]=i[1];
+                coCount[i[1]]++;
+            }
+            else
+            {
+                coCount[co[i[0]]]--;
+                if(coCount[co[i[0]]]==0)
+                coCount.erase(co[i[0]]);
+                co[i[0]]=i[1];
+                coCount[i[1]]++;
+            }
+            ans.push_back(coCount.size());
+        }
+        return ans;
+    }
+};
