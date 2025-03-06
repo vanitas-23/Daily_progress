324363ed0116dd287293c3047bb4dc4e13b87cef
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-03-06 13:40:52 +0530

    Time: 0 ms (100.00%) | Memory: 25.6 MB (95.09%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        long long sum = 0, sqrSum = 0;
+        long long n = grid.size();
+        long long total = n * n;
+        for (int row = 0; row < n; ++row) {
+            for (int col = 0; col < n; ++col) {
+                sum += grid[row][col];
+                sqrSum +=(grid[row][col]) * 1ll * grid[row][col];
+            }
+        }
+        long long sumDiff = sum - total * (total + 1) / 2;
+        long long sqrDiff = sqrSum - total * (total + 1) * (2 * total + 1) / 6;
+        int repeat = (sqrDiff / sumDiff + sumDiff) / 2;
+        int missing = (sqrDiff / sumDiff - sumDiff) / 2;
+
+        return {repeat, missing};
+    }
+};
