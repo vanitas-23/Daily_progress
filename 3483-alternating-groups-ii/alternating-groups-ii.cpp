b152bde7bc47504c3e1dfab40c478f5ad95dc3a3
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-03-09 12:12:08 +0530

    Time: 371 ms (5.02%) | Memory: 113 MB (13.39%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& c, int k) {
+        int ans=0;
+        vector<int>arr(c.begin(),c.end());
+        for(int i=0;i<k-1;i++)
+        arr.push_back(c[i]);
+        int sz=1;
+        for(auto i:arr)
+        cout<<i<<" ";
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]!=arr[i-1])
+            sz++;
+            else
+            {
+                if(sz>=k)
+                ans+=sz-k+1;
+                sz=1;
+            }
+        }
+        if(sz>=k)
+        ans+=sz-k+1;
+        return ans;
+    }
+};
