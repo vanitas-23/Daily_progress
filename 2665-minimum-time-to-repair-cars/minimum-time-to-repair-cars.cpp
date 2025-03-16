5c7b8e3083d923ca36d7db512294f0bb5f4d64df
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-03-16 11:26:57 +0530

    Time: 58 ms (40.11%) | Memory: 60.5 MB (5.08%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -0,0 +1,34 @@
+#define ll long long
+class Solution {
+public:
+    bool f(ll mid,vector<int>& ranks, int cars)
+    {
+        for(auto i:ranks)
+        {
+            ll x=mid/i;
+            x=sqrtl(x);
+            //cout<<i<<" "<<x<<endl;
+            cars-=x;
+            if(cars<=0)
+            return 1;
+        }
+        return 0;
+    }
+    long long repairCars(vector<int>& ranks, int cars) {
+        ll l=0;
+        ll r=1e15;
+        ll ans=r;
+        while(l<=r)
+        {
+            ll mid=r-(r-l)/2;
+            if(f(mid,ranks,cars))
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else
+            l=mid+1;
+        }
+        return ans;
+    }
+};
