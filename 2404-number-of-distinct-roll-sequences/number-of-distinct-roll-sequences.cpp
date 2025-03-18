c1298e202046645d1d8a1815cd56052c61d3b43a
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-03-18 23:40:22 +0530

    Time: 66 ms (95.68%) | Memory: 11.2 MB (70.99%) - LeetSync

--- 2404-number-of-distinct-roll-sequences/number-of-distinct-roll-sequences.cpp
+++ 2404-number-of-distinct-roll-sequences/number-of-distinct-roll-sequences.cpp
@@ -1,34 +1,30 @@
-int mod=1e9+7;
+int MOD = 1e9 + 7;
+int dp[10001][7][7];
 class Solution {
 public:
-    int f(int i,int n,map<int,vector<int>>& mp,int last,int plast,vector<vector<vector<int>>>& dp)
-    {
-        if(i==n)
-        return 1;
-        if(dp[i][last][plast]!=-1)
-        return dp[i][last][plast];
-        auto x=mp[last];
-        int ans=0;
-        int newmask=0;
-        for(auto j:x)
-        {
-            if(j==plast) continue;
-            ans=(ans%mod+f(i+1,n,mp,j,last,dp)%mod)%mod;
+    int f(int i, int n, vector<vector<int>>& mp, int last, int plast) {
+        if (i == n) return 1;
+        if (dp[i][last][plast] != -1) return dp[i][last][plast];
+
+        int ans = 0;
+        for (auto j : mp[last]) {
+            if (j == plast) continue;
+            ans = (ans + f(i + 1, n, mp, j, last)) % MOD;
         }
-        return dp[i][last][plast]=ans;
+        return dp[i][last][plast] = ans;
     }
+
     int distinctSequences(int n) {
-        map<int,vector<int>>mp;
-        mp[1]={2,3,4,5,6};
-        mp[2]={1,3,5};
-        mp[3]={1,2,4,5};
-        mp[4]={1,3,5};
-        mp[5]={1,2,3,4,6};
-        mp[6]={1,5};
-        int ans=0;
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(7,vector<int>(7,-1)));
-        for(int i=1;i<=6;i++)
-        ans=(ans%mod+f(1,n,mp,i,0,dp)%mod)%mod;
+        vector<vector<int>> mp = {
+            {}, {2, 3, 4, 5, 6}, {1, 3, 5}, {1, 2, 4, 5}, 
+            {1, 3, 5}, {1, 2, 3, 4, 6}, {1, 5}
+        };
+
+        memset(dp, -1, sizeof(dp));
+        int ans = 0;
+        for (int i = 1; i <= 6; i++) {
+            ans = (ans + f(1, n, mp, i, 0)) % MOD;
+        }
         return ans;
     }
 };
