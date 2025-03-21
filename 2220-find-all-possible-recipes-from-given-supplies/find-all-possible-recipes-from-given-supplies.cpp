7f03c34e742909385d72ae7e9a52067f5b391520
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-03-21 20:51:53 +0530

    Time: 335 ms (20.28%) | Memory: 165.6 MB (27.80%) - LeetSync

---
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& rec, vector<vector<string>>& ing, vector<string>& supp) {
+        queue<string> q;
+        map<string, vector<string>> mp;
+        map<string,int>inD;
+        set<string>st;
+        vector<string>res;
+        int n=rec.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<ing[i].size();j++)
+            {
+                mp[ing[i][j]].push_back(rec[i]);
+                inD[rec[i]]++;
+            }
+            st.insert(rec[i]);
+        }
+        for(auto i:supp)
+        q.push(i);
+        while(!q.empty())
+        {
+            auto x=q.front();
+            q.pop();
+            if(st.find(x)!=st.end())
+            {
+                res.push_back(x);
+                st.erase(x);
+            }
+            for(auto i:mp[x])
+            {
+                inD[i]--;
+                if(inD[i]==0)
+                q.push(i);
+            }
+        }
+        return res;
+    }
+};
