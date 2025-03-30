2a8434b69b026bc4a60b1217998761aa34d28ab3
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-03-30 23:47:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (88.22%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int>frq(26);
-        for(char c:s)
-        frq[c-'a']++;
-        vector<int>res;
-        string temp="";
-        for(char c:s)
+        int n=s.size();
+        vector<int>lst(26);
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        lst[s[i]-'a']=i;
+        int mx=-1;
+        int f=-1;
+        for(int i=0;i<n;i++)
         {
-            temp+=c;
-            frq[c-'a']--;
-            bool b=1;
-            for(char l:temp)
-            if(frq[l-'a']>0)
-            {
-                b=0;
-                break;
-            }
-            if(!b)
-            continue;
-            res.push_back(temp.size());
-            temp="";
+           mx=max(mx,lst[s[i]-'a']);
+           if(mx==i)
+           ans.push_back(i-f),f=i;
         }
-        return res;
+        return ans;
     }
 };
