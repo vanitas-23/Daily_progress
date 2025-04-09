0a34dac29d7a2be62831ec97ecfc3b094529092f
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-04-09 23:21:34 +0530

    Time: 20 ms (67.52%) | Memory: 238.1 MB (28.24%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -1,46 +1,66 @@
-class Solution {
-    private:
-    int getcmpcost(int node, vector<vector<pair<int,int>>>&adj,vector<bool>&vis,vector<int>&cmp,int cid){
-        queue<int>q;
-        int ccost=INT_MAX;
-        q.push(node);
-        vis[node]=1;
-        while(!q.empty()){
-            int Node=q.front();
-            q.pop();
-            cmp[Node]=cid;
-            for(auto [nbr,w]:adj[Node]){
-                ccost&=w;
-                if(vis[nbr])continue;
-                vis[nbr]=1;
-                q.push(nbr);
-            }
-        }
-        return ccost;
-    }
+class DisjointSet {
 public:
-    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        vector<vector<pair<int,int>>>adj(n);
-        for(auto ele:edges){
-            adj[ele[0]].push_back({ele[1],ele[2]});
-            adj[ele[1]].push_back({ele[0],ele[2]});
-        }
-        vector<bool>vis(n,0);
-        vector<int>cmp(n);
-        vector<int>cmpcost;
-        int cid=0;
-        for(int node=0;node<n;node++){
-            if(!vis[node]){
-                cmpcost.push_back(getcmpcost(node,adj,vis,cmp,cid));
-                cid++;
+    DisjointSet(int n) : N(n), sz(n), pars(n, -1) {}
+    
+    void reset() { sz = N, fill(pars.begin(), pars.end(), -1); }
+
+    int find(int x) { return pars[x] < 0 ? x : pars[x] = find(pars[x]); }
+
+    int size() { return sz; }
+
+    int count(int x) { return -pars[find(x)]; }
+
+    bool unite(int x, int y) {
+        x = find(x), y = find(y);
+        if (x == y) return false;
+        --sz;
+        if (pars[x] < pars[y]) swap(x, y);
+        pars[y] += pars[x];
+        pars[x] = y;
+        return true;
+    }
+
+    bool isSame(int x, int y) { return find(x) == find(y); }
+
+private:
+    int N, sz;
+    vector<int> pars;
+};
+
+class Solution {
+public:
+    vector<int> minimumCost(int n, vector<vector<int>>& e, vector<vector<int>>& q) {
+        vector<int> ans;
+        DisjointSet ds(n);
+        vector<int> ands(n, -1);
+
+        for (auto& edge : e) {
+            int a = edge[0], b = edge[1], w = edge[2];
+            int ra = ds.find(a);
+            int rb = ds.find(b);
+
+            if (ra != rb) {
+                ds.unite(a, b);
+                int r = ds.find(a);
+                if (ands[ra] == -1) ands[ra] = w;
+                else ands[ra] &= w;
+                if (ands[rb] == -1) ands[rb] = w;
+                else ands[rb] &= w;
+                ands[r] = ands[ra] & ands[rb]; 
+            } else {
+                int r = ra;
+                if (ands[r] == -1) ands[r] = w;
+                else ands[r] &= w;
             }
         }
-        vector<int>ans;
-        for(auto q:query){
-            int s=q[0];
-            int e=q[1];
-            if(cmp[s]==cmp[e])ans.push_back(cmpcost[cmp[s]]);
-            else ans.push_back(-1);
+
+        for (auto& qu : q) {
+            int a = qu[0], b = qu[1];
+            if (ds.isSame(a, b)) {
+                ans.push_back(ands[ds.find(a)]);
+            } else {
+                ans.push_back(-1);
+            }
         }
         return ans;
     }
