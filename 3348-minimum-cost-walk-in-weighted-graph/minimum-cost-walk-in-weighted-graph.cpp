e09074da1e2b90da79f57bb422c218c49afabc86
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-04-09 23:26:42 +0530

    Time: 68 ms (56.14%) | Memory: 246.8 MB (20.06%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -1,66 +1,50 @@
+
 class DisjointSet {
-public:
-    DisjointSet(int n) : N(n), sz(n), pars(n, -1) {}
-    
-    void reset() { sz = N, fill(pars.begin(), pars.end(), -1); }
-
-    int find(int x) { return pars[x] < 0 ? x : pars[x] = find(pars[x]); }
-
-    int size() { return sz; }
-
-    int count(int x) { return -pars[find(x)]; }
-
-    bool unite(int x, int y) {
-        x = find(x), y = find(y);
-        if (x == y) return false;
-        --sz;
-        if (pars[x] < pars[y]) swap(x, y);
-        pars[y] += pars[x];
-        pars[x] = y;
-        return true;
-    }
-
-    bool isSame(int x, int y) { return find(x) == find(y); }
-
-private:
-    int N, sz;
-    vector<int> pars;
+    public:
+        DisjointSet(int n) : N(n), sz(n), pars(n, -1) {
+        }
+        void reset() { sz = N, fill(pars.begin(), pars.end(), -1); }
+        int find(int x) { return pars[x] < 0 ? x : pars[x] = find(pars[x]); }
+        int size() { return sz; }
+        int count(int x) { return -pars[find(x)]; }
+        bool unite(int x, int y) {
+            x = find(x), y = find(y);
+            if (x == y) { return false; }
+            --sz;
+            if (pars[x] < pars[y]) { swap(x, y); }
+            pars[y] += pars[x];
+            pars[x] = y;
+            return true;
+        }
+        bool isSame(int x, int y) { return find(x) == find(y); }
+    private:
+        int N, sz;
+        vector<int> pars;
 };
-
 class Solution {
 public:
     vector<int> minimumCost(int n, vector<vector<int>>& e, vector<vector<int>>& q) {
-        vector<int> ans;
+        vector<int>ans;
         DisjointSet ds(n);
-        vector<int> ands(n, -1);
-
-        for (auto& edge : e) {
-            int a = edge[0], b = edge[1], w = edge[2];
-            int ra = ds.find(a);
-            int rb = ds.find(b);
-
-            if (ra != rb) {
-                ds.unite(a, b);
-                int r = ds.find(a);
-                if (ands[ra] == -1) ands[ra] = w;
-                else ands[ra] &= w;
-                if (ands[rb] == -1) ands[rb] = w;
-                else ands[rb] &= w;
-                ands[r] = ands[ra] & ands[rb]; 
-            } else {
-                int r = ra;
-                if (ands[r] == -1) ands[r] = w;
-                else ands[r] &= w;
-            }
+        map<int,int>mp;
+        for(int i=0;i<e.size();i++)
+        {
+            int a=e[i][0],b=e[i][1];
+            ds.unite(a,b);
         }
-
-        for (auto& qu : q) {
-            int a = qu[0], b = qu[1];
-            if (ds.isSame(a, b)) {
-                ans.push_back(ands[ds.find(a)]);
-            } else {
-                ans.push_back(-1);
-            }
+        for(int i=0;i<e.size();i++)
+        {
+            if(mp.find(ds.find(e[i][0]))==mp.end())
+            mp[ds.find(e[i][0])]=e[i][2];
+            else
+            mp[ds.find(e[i][0])]&=e[i][2];
+        }
+        for(auto i:q)
+        {
+            if(ds.isSame(i[0],i[1]))
+            ans.push_back(mp[ds.find(i[0])]);
+            else
+            ans.push_back(-1);
         }
         return ans;
     }
