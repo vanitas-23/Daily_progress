82fd9fd5c9ab342e5e4ed00379057ca665e10f7e
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-04-19 17:40:48 +0530

    Time: 65 ms (21.14%) | Memory: 60.5 MB (38.74%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -3,12 +3,14 @@ public:
     long long f(int target, int n, vector<int>& nums) {
         long long ans = 0;
         for (int i = 0; i < n; i++) {
-            int it = upper_bound(nums.begin() + i + 1, nums.end(), target - nums[i]) - nums.begin();
+            int it = upper_bound(nums.begin() + i + 1, nums.end(),
+                                 target - nums[i]) -
+                     nums.begin();
             ans += (it - i - 1);
         }
         return ans;
     }
-    
+
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
