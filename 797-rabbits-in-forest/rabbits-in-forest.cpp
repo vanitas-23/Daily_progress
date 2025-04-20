2c129b5b0c06e24a305889cc6f366debddec19a1
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-04-20 12:00:39 +0530

    Time: 2 ms (28.54%) | Memory: 12.3 MB (10.59%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& ans) {
+        int res=0;
+        map<int,int>mp;
+        for(auto i:ans)
+        {
+            if(mp.find(i)==mp.end())
+            {
+                res+=(i+1);
+                if(i!=0)
+                mp[i]=i;
+            }
+            else
+            {
+                mp[i]--;
+                if(mp[i]==0)
+                mp.erase(i);
+            }
+        }
+        return res;
+    }
+};
