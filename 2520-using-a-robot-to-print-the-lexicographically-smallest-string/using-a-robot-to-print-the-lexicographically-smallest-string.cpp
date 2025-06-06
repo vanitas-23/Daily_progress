2dd600f76642082bf0e34311a1aa877f62e46104
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-06-06 23:51:51 +0530

    Time: 121 ms (20.30%) | Memory: 34 MB (55.45%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -1,47 +1,25 @@
 class Solution {
 public:
     string robotWithString(string s) {
-        
-        int n=s.size();
-        string ans;
-         vector<int> post(n);
-         int i;
-         post[n-1]=n-1;
-         int c=n-1;
-         for(i=n-2;i>=0;i--){
-             
-             if(s[i]<=s[c]){
-                 c=i;
-             }
-             post[i]=c;
-           
-         }
-
-         int curr=0;
-            string t;
-
-            while(curr<n){
-            
-             int c=post[curr];
-             while(t.size()>0&&t[t.size()-1]<=s[c]){
-                ans.push_back(t[t.size()-1]);
-                t.pop_back();
-
-             }
-             for(int i=curr;i<=c;i++){
-                 t.push_back(s[i]);
-             }
-             if(c==n-1){
-                 break;
-             }
-             curr=c+1;
-
+        unordered_map<char, int> cnt;
+        for (char c : s) {
+            cnt[c]++;
+        }
+        stack<char> stk;
+        string res;
+        char minCharacter = 'a';
+        for (char c : s) {
+            stk.emplace(c);
+            cnt[c]--;
+            while (minCharacter != 'z' && cnt[minCharacter] == 0) {
+                minCharacter++;
             }
-            for(int i=t.size()-1;i>=0;i--){
-               ans.push_back(t[i]);
+            while (!stk.empty() && stk.top() <= minCharacter) {
+                res.push_back(stk.top());
+                stk.pop();
             }
+        }
 
-         return ans;
-
+        return res;
     }
 };
