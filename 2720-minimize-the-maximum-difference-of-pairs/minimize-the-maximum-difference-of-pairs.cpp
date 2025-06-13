704b50cf980d389d1b0f14f458d194f2310a46bd
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-06-13 21:21:49 +0530

    Time: 28 ms (43.42%) | Memory: 86.8 MB (50.22%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    bool can(vector<int>& nums,int mid,int p,int n)
+    bool f(int mid,int n,vector<int>& nums,int p)
     {
-        int cnt=0;
         int i=0;
         while(i<n-1)
         {
             if(nums[i+1]-nums[i]<=mid)
-            cnt++,i++;
+            p--,i++;
+            if(p==0)
+            return 1;
             i++;
         }
-        return cnt>=p;
+        return 0;
     }
     int minimizeMax(vector<int>& nums, int p) {
+        if(p==0)
+        return 0;
         sort(nums.begin(),nums.end());
         int n=nums.size();
         int l=0;
-        int r=nums.back()-nums[0];
+        int r=nums[n-1]-nums[0];
         int ans=r;
         while(l<=r)
         {
-            int mid=(l+r)/2;
-            if(can(nums,mid,p,n))
+            int mid=(r-(r-l)/2);
+            if(f(mid,n,nums,p))
             {
                 ans=mid;
                 r=mid-1;
