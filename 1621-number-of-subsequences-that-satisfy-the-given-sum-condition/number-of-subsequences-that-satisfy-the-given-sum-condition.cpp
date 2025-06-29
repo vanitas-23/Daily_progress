fc50fbac7d474fcaa9519665ea0dfb963396eee1
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-06-29 13:07:17 +0530

    Time: 54 ms (9.27%) | Memory: 51.4 MB (95.04%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,28 +1,32 @@
-const int MOD=1e9+7;
 class Solution {
 public:
-    int mod_expo(int a,int b)
+    int mod_expo(int a,int b,const int& MOD)
     {
         if(b==0)
         return 1;
         if(b==1)
         return a;
-        long long x=mod_expo(a,b/2);
+        long long x=mod_expo(a,b/2,MOD);
         x=(x%MOD * x%MOD+MOD)%MOD;
         if(b%2)
         return (x % MOD * a % MOD + MOD)%MOD;
         return x;
     }
-    int numSubseq(vector<int>& nums, int k) {
-       sort(nums.begin(),nums.end());
-       int ans=0;
-       int n=nums.size();
-       for(int i=0;i<n;i++)
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
+        int ans=0;
+        const int MOD=1e9+7;
+        while(i<=j)
         {
-            int id=upper_bound(nums.begin(),nums.end(),-nums[i]+k)-nums.begin();
-            id--;
-            if(id>=i)
-            ans=(ans%MOD+mod_expo(2,id-i))%MOD;
+            if(nums[i]+nums[j]>target)
+            j--;
+            else
+            {
+                ans=(ans%MOD+mod_expo(2,j-i,MOD)%MOD)%MOD;
+                i++;
+            }
         }
         return ans;
     }
