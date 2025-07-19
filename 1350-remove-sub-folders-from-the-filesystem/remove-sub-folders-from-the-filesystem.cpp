f37810f2c26af24d50e72246e6aff2670b7e7ad3
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-07-19 10:08:20 +0530

    Time: 61 ms (61.08%) | Memory: 55 MB (53.55%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    vector<string> removeSubfolders(vector<string>& folder) {
-        // Create a set to store all folder paths for fast lookup
-        unordered_set<string> folderSet(folder.begin(), folder.end());
-        vector<string> result;
-
-        // Iterate through each folder to check if it's a sub-folder
-        for (string& f : folder) {
-            bool isSubFolder = false;
-            string prefix = f;
-
-            // Check all prefixes of the current folder path
-            while (!prefix.empty()) {
-                size_t pos = prefix.find_last_of('/');
-                if (pos == string::npos) break;
-
-                // Reduce the prefix to its parent folder
-                prefix = prefix.substr(0, pos);
-
-                // If the parent folder exists in the set, mark as sub-folder
-                if (folderSet.count(prefix)) {
-                    isSubFolder = true;
+    vector<string> removeSubfolders(vector<string>& fol) {
+        sort(fol.begin(),fol.end());
+        unordered_set<string>st;
+        for(auto i:fol)
+        {
+            string temp="";
+            bool b=0;
+            for(int j=0;j<i.size();j++)
+            {
+                temp+=i[j];
+                if(j==i.size()-1 || (i[j+1]=='/'))
+                if(st.find(temp)!=st.end())
+                {
+                    b=1;
                     break;
                 }
             }
-
-            // If not a sub-folder, add it to the result
-            if (!isSubFolder) {
-                result.push_back(f);
-            }
+            if(!b)
+            st.insert(i);
         }
-
-        return result;
+        vector<string>res(st.begin(),st.end());
+        return res;
     }
 };
