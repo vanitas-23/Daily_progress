8446d2aee4090b616588d30fab76f691b5580b24
Syed Kafilur Rehman <125473809+vanitas-23@users.noreply.github.com>
2025-08-09 11:40:25 +0530

    Time: 125 ms (24.58%) | Memory: 428 MB (25.08%) - LeetSync

---
+++ 826-soup-servings/soup-servings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double f(int a,int b, vector<vector<double>>& dp){
+        if(a<=0 && b<=0)
+        return 0.5;
+        if(a<=0)
+        return 1.0;
+        if(b<=0)
+        return 0;
+        if(dp[a][b]!=-1)
+        return dp[a][b];
+        double ans= f(a-100,b,dp)+f(a-75,b-25,dp)+f(a-50,b-50,dp)+f(a-25,b-75,dp);
+        return dp[a][b]=0.25*ans;
+    }
+    double soupServings(int n) {
+        if(n>=4800)
+        return 1;
+        vector<vector<double>>dp(n+1,vector<double>(n+1,-1));
+        double ans=f(n,n,dp);
+       
+        return ans;
+    }
+};
